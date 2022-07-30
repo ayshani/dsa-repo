53316bd2c0b3bb7e36f557a2da9f558a5eb001d0
conjuring420 <conjuring420@outlook.com>
2022-07-30 21:31:51 +0530

    commit

--- dsa-repo/src/com/slidingwindow/NumberOfSubstringsContainingAllThreeCharacters.java
+++ dsa-repo/src/com/slidingwindow/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -42,16 +42,13 @@ public class NumberOfSubstringsContainingAllThreeCharacters {
     public int numberOfSubstrings(String s) {
         int[] count =new int[]{0,0,0};
         int res =0,i=0;
-        for(int j=0;j<s.length();j++){
-            count[s.charAt(j)-'a']++;
-
-            while(count[0]>0 && count[1]>0 && count[2]>0){
-                count[s.charAt(i++)-'a']--;
+        for(int j=0;j<s.length();j++) {
+            count[s.charAt(j) - 'a']++;
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                count[s.charAt(i++) - 'a']--;
             }
-
-            res+=i;
+            res += i;
         }
-
         return res;
     }
 
