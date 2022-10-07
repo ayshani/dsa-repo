5bd105da3aacf85a67d124c3f2ec5ae18deaf334
Ayshani Malik <ayshanim@gmail.com>
2022-10-07 10:28:49 +0530

    Add

---
+++ dsa-repo/src/com/design/FindingPairsWithACertainSum.java
@@ -0,0 +1,92 @@
+package com.design;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+1865. Finding Pairs With a Certain Sum
+
+You are given two integer arrays nums1 and nums2. You are tasked to implement a data structure that supports
+queries of two types:
+
+Add a positive integer to an element of a given index in the array nums2.
+Count the number of pairs (i, j) such that nums1[i] + nums2[j] equals a given value (0 <= i < nums1.length
+and 0 <= j < nums2.length).
+Implement the FindSumPairs class:
+
+FindSumPairs(int[] nums1, int[] nums2) Initializes the FindSumPairs object with two integer arrays nums1 and nums2.
+void add(int index, int val) Adds val to nums2[index], i.e., apply nums2[index] += val.
+int count(int tot) Returns the number of pairs (i, j) such that nums1[i] + nums2[j] == tot.
+
+
+Example 1:
+
+Input
+["FindSumPairs", "count", "add", "count", "count", "add", "add", "count"]
+[[[1, 1, 2, 2, 2, 3], [1, 4, 5, 2, 5, 4]], [7], [3, 2], [8], [4], [0, 1], [1, 1], [7]]
+Output
+[null, 8, null, 2, 1, null, null, 11]
+
+Explanation
+FindSumPairs findSumPairs = new FindSumPairs([1, 1, 2, 2, 2, 3], [1, 4, 5, 2, 5, 4]);
+findSumPairs.count(7);  // return 8; pairs (2,2), (3,2), (4,2), (2,4), (3,4), (4,4) make 2 + 5
+                           and pairs (5,1), (5,5) make 3 + 4
+findSumPairs.add(3, 2); // now nums2 = [1,4,5,4,5,4]
+findSumPairs.count(8);  // return 2; pairs (5,2), (5,4) make 3 + 5
+findSumPairs.count(4);  // return 1; pair (5,0) makes 3 + 1
+findSumPairs.add(0, 1); // now nums2 = [2,4,5,4,5,4]
+findSumPairs.add(1, 1); // now nums2 = [2,5,5,4,5,4]
+findSumPairs.count(7);  // return 11; pairs (2,1), (2,2), (2,4), (3,1), (3,2), (3,4), (4,1), (
+ */
+public class FindingPairsWithACertainSum {
+    public static void main(String[] args) {
+        FindSumPairs findSumPairs = new FindSumPairs(new int[]{1, 1, 2, 2, 2, 3}, new int[]{1, 4, 5, 2, 5, 4});
+
+        System.out.println(findSumPairs.count(7));
+        findSumPairs.add(3, 2);
+        System.out.println(findSumPairs.count(8));
+        System.out.println(findSumPairs.count(4));
+        findSumPairs.add(0, 1);
+        findSumPairs.add(1, 1);
+        System.out.println(findSumPairs.count(7));
+    }
+
+}
+
+class FindSumPairs {
+
+    int[] num1,num2;
+    Map<Integer,Integer> frequencyMap;
+
+    //o(n)
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.num1 = nums1;
+        this.num2 = nums2;
+        frequencyMap = new HashMap<>();
+        for(int num : this.num2)
+        {
+            updateFrequencey(num,1);
+        }
+    }
+
+    private void updateFrequencey(int num, int inc){
+        frequencyMap.put(num, frequencyMap.getOrDefault(num,0)+inc);
+    }
+
+    //o(1)
+    public void add(int index, int val) {
+        updateFrequencey(num2[index],-1);
+        num2[index] += val;
+        updateFrequencey(num2[index],1);
+    }
+
+    //o(n)
+    public int count(int tot) {
+        int ans =0;
+        for(int num : num1){
+            ans+= frequencyMap.getOrDefault(tot - num,0);
+        }
+
+        return ans;
+    }
+}
