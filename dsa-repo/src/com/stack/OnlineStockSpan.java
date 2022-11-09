e3e0d8e4dd0faa5ed51a510935409079fc1ff829
Ayshani Malik <ayshanim@gmail.com>
2022-11-09 22:29:00 +0530

    Add

---
+++ dsa-repo/src/com/stack/OnlineStockSpan.java
@@ -0,0 +1,71 @@
+package com.stack;
+
+import java.util.Stack;
+
+/*
+901. Online Stock Span
+
+Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backward) for which the stock price was less than or equal to today's price.
+
+For example, if the price of a stock over the next 7 days were [100,80,60,70,60,75,85],
+then the stock spans would be [1,1,1,2,1,4,6].
+Implement the StockSpanner class:
+
+StockSpanner() Initializes the object of the class.
+int next(int price) Returns the span of the stock's price given that today's price is price.
+
+
+Example 1:
+
+Input
+["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+[[], [100], [80], [60], [70], [60], [75], [85]]
+Output
+[null, 1, 1, 1, 2, 1, 4, 6]
+
+Explanation
+StockSpanner stockSpanner = new StockSpanner();
+stockSpanner.next(100); // return 1
+stockSpanner.next(80);  // return 1
+stockSpanner.next(60);  // return 1
+stockSpanner.next(70);  // return 2
+stockSpanner.next(60);  // return 1
+stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were
+less than or equal to today's price.
+stockSpanner.next(85);  // return 6
+
+TC : o(1)
+SC : o(n)
+ */
+public class OnlineStockSpan {
+    public static void main(String[] args) {
+        StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.next(100));
+        System.out.println(stockSpanner.next(80));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(70));
+        System.out.println(stockSpanner.next(60));
+        System.out.println(stockSpanner.next(75));
+        System.out.println(stockSpanner.next(85));
+    }
+}
+
+class StockSpanner {
+    Stack<int[]> stack;
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+        int ans =1;
+        while(!stack.isEmpty() && stack.peek()[0]<=price){
+            ans+= stack.pop()[1];
+        }
+
+        stack.push(new int[]{price,ans});
+        return ans;
+    }
+}
+
