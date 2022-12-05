99608b39b3d7a5512896d3dbb443b3ad682e68a0
Ayshani Malik <ayshanim@gmail.com>
2022-12-05 20:57:11 +0530

    Add

--- dsa-repo/src/com/linkedlist/MiddleOfTheLinkedList.java
+++ dsa-repo/src/com/linkedlist/MiddleOfTheLinkedList.java
@@ -34,6 +34,7 @@ public class MiddleOfTheLinkedList {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head, fast = head;
 
+
         while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow = slow.next;
