b4713af1e2e73cc704e05cdcfb3cf3bec86e49bd
Ayshani Malik <ayshanim@gmail.com>
2022-12-18 14:34:32 +0530

    Add

---
+++ dsa-repo/src/com/stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,43 @@
+package com.stack;
+
+import java.util.Stack;
+
+/*
+150. Evaluate Reverse Polish Notation
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+Note that division between two integers should truncate toward zero.
+
+It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate
+to a result, and there will not be any division by zero operation.
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+
+TC : o(n)
+SC : o(n)
+ */
+public class EvaluateReversePolishNotation {
+    public static void main(String[] args) {
+        String[] nums = new String[]{"2","1","+","3","*"};
+        System.out.println(new EvaluateReversePolishNotation().evalRPN(nums));
+    }
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if ("+".equals(s)) stack.push(stack.pop() + stack.pop());
+            else if ("-".equals(s)) stack.push(-stack.pop() + stack.pop());
+            else if ("/".equals(s)) stack.push((int)(1D / stack.pop() * stack.pop()));
+            else if ("*".equals(s)) stack.push(stack.pop() * stack.pop());
+            else stack.push(Integer.valueOf(s));
+        }
+
+        return stack.pop();
+    }
+}
