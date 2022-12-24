79496241a23f7b3314f07576be46eedf6048a2e0
Ayshani Malik <ayshanim@gmail.com>
2022-12-25 00:08:54 +0530

    Add

--- dsa-repo/src/com/dp/DominoAndTrominoTiling.java
+++ dsa-repo/src/com/dp/DominoAndTrominoTiling.java
@@ -64,49 +64,86 @@ We will multiply by p(k−1) by 2 because for any partially covered tiling,
 Summing the ways to reach f(k) gives us the following equation:
 
 f(k) = f(k-1) + f(k-2) + 2 * p(k-1)
+
+Now that we know where tilings on f(k) are coming from, how about p(k)?
+Can we apply the same logic and find that out? Absolutely yes!
+
+Take a pen and start drawing scenarios that contribute to p(4)
+(this is a good technique to aid critical thinking during an interview).
+Start by drawing p(4), remember p(4) is a board of width 4 with the first 3 columns fully covered
+and the last column half covered. Now, try removing a domino or a tromino to find which scenarios
+contribute to p(4). Notice that p(k) can come from the below scenarios:
+
+Adding a tromino to a fully covered board of width k−2 (i.e. f(k−2))
+Adding a horizontal domino to a partially covered board of width k−1 (i.e. p(k−1))
+Thus, we arrive at the following conclusion for p(k):
+
+p(k) = p(k-1) + f(k-2)
+
+
+Intuition
+
+One of the drawbacks to the previous top-down DP solution is that it uses a recursive call
+stack which requires additional time and space to maintain. When we know that all of the subproblems
+(i.e. f(1) through f(n−1)) must be solved and there is a logical order to the subproblems (i.e. f(1)
+must be solved before f(2), and f(2) before ... f(n−1)), then bottom-up DP will generally be more efficient
+than top-down DP because it will solve the same number of subproblems and do so without maintaining a call stack.
+Different from the previous top-down solution, this solution will be in a bottom-up fashion. As such,
+we will start by calculating the base case (when nnn is very small, like 0, 1, 2 etc.), then move to
+the next case (when nnn grows to a larger number) and gradually get the result for the final case f(n).
+
+Algorithm
+
+Create two arrays, f and p, of size n+1, where f(k) represents the number of ways to fully cover a board of width k
+ and p(k) represents the number of ways to partially cover a board of width k (as described in the overview).
+Initialize f and p according to the following base cases:
+f(1)=1 because to fully cover a board of width 1, there is only one way, add one vertical domino.
+f(2)=2 because to fully cover a board of width 2, there are two ways, either add two horizontal dominos or
+    add two vertical dominos.
+p(2)=1 because to partially cover a board of width 2, there is only one way using an L-shaped tromino
+    (leave the upper-right corner uncovered).
+Iterate k from 2 to n (inclusive) and at each iteration update f and p according to the transition functions
+we derived in the overview:
+f(k) = f(k-1) + f(k-2) + 2 * p(k-1)
+p(k) = p(k-1) + f(k-2)
+Return f(n) which now represents the number of ways to fully cover a board of width n.
+
+Complexity Analysis
+
+Let NNN be the width of the board.
+
+Time complexity: O(N)
+
+Array iteration requires N−2 iterations where each iteration takes constant time.
+
+Space complexity: O(N)
+
+Two arrays of size N+1 are used to store the number of ways to fully and partially tile boards of
+various widths between 1 and N.
  */
 public class DominoAndTrominoTiling {
-
-    int MOD = 1_000_000_007;
-    Map<Integer, Long> f_cache = new HashMap<>();
-    Map<Integer, Long> p_cache = new HashMap<>();
-
 
     public static void main(String[] args) {
         System.out.println(new DominoAndTrominoTiling().numTilings(3));
     }
-    public long p(int n) {
-        if (p_cache.containsKey(n)) {
-            return p_cache.get(n);
-        }
-        long val;
-        if (n == 2) {
-            val = 1L;
-        } else {
-            val = (p(n - 1) + f(n - 2)) % MOD;
-        }
-        p_cache.put(n, val);
-        return val;
-
-    }
-
-    public long f(int n) {
-        if (f_cache.containsKey(n)) {
-            return f_cache.get(n);
-        }
-        long val;
-        if (n == 1) {
-            val = 1L;
-        } else if (n == 2) {
-            val = 2L;
-        } else {
-            val = (f(n - 1) + f(n - 2) + 2 * p(n - 1)) % MOD;
-        }
-        f_cache.put(n, val);
-        return val;
-    }
-
     public int numTilings(int n) {
-        return (int) (f(n));
+        int MOD = 1_000_000_007;
+        // handle base case scenarios
+        if (n <= 2) {
+            return n;
+        }
+        // f[k]: number of ways to "fully cover a board" of width k
+        long[] f = new long[n + 1];
+        // p[k]: number of ways to "partially cover a board" of width k
+        long[] p = new long[n + 1];
+        // initialize f and p with results for the base case scenarios
+        f[1] = 1L;
+        f[2] = 2L;
+        p[2] = 1L;
+        for (int k = 3; k < n + 1; ++k) {
+            f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % MOD;
+            p[k] = (p[k - 1] + f[k - 2]) % MOD;
+        }
+        return (int) (f[n]);
     }
 }
