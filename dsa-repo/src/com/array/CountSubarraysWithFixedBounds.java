a2db3738bd56993f60e479e97da2d341a8e07f00
Ayshani Malik <ayshanim@gmail.com>
2023-01-17 23:11:49 +0530

    Add

---
+++ dsa-repo/src/com/array/CountSubarraysWithFixedBounds.java
@@ -0,0 +1,74 @@
+package com.array;
+/*
+2444. Count Subarrays With Fixed Bounds
+
+You are given an integer array nums and two integers minK and maxK.
+
+A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
+
+The minimum value in the subarray is equal to minK.
+The maximum value in the subarray is equal to maxK.
+Return the number of fixed-bound subarrays.
+
+A subarray is a contiguous part of an array.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+Output: 2
+Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+ */
+public class CountSubarraysWithFixedBounds {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,3,5,2,7,5};
+        System.out.println(new CountSubarraysWithFixedBounds().countSubarrays(nums,1,5));
+    }
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int minIndex =-1, maxIndex =-1, startNextSubArray =0;
+        long count =0;
+        for(int i=0;i<nums.length;i++){
+            /*
+            if nums[i] is out of bound,
+            then we reset index to 0 and next subarray will start from i+1
+            */
+            if(nums[i] <minK || nums[i] >maxK){
+                minIndex = 0;
+                maxIndex =0;
+                startNextSubArray = i+1;
+            }
+
+            //if nums[i] matches with min bound, store index
+            if(nums[i] == minK)
+                minIndex =i;
+
+            //if nums[i] matches with max bound, store index
+            if(nums[i] == maxK)
+                maxIndex =i;
+
+            /*
+            it is possible that minBound is on right side and maxbound in left side
+            i.e. minK =1, maxK =7   [9,4,3,7,5,1]
+            Hence we take minIndex from this two.
+                 0 1 2 3 4 5 -- Index
+                [9,4,3,7,5,1]
+            startNextSubArray = 1
+            through iteration, unless we reach to last index, count remains 0 for this example.
+            once we get minIndex = 5, maxIndex  =3,
+            we consider the minimum value between this two i.e. maxIndex =3.
+            now 3 - 1 +1 = 3 is the count.
+            subarrays are : [4,3,7..] , [3,7..] , [7..]
+            .. - this is because we consider only the min value of maxIndex and MinIndex
+                as if we get two, then the subarray is already present,now we need to only check
+                the extras i.e. left side and right side of the subarray whether
+                it falls inside the bound.
+                    if it falls, we count else the indices got resetted.
+            */
+            count  += Math.max(0L,Math.min(minIndex,maxIndex) - startNextSubArray+1);
+        }
+
+        return count;
+    }
+}
---
+++ dsa-repo/src/com/tree/AddEdgesToMakeDegreesOfAllNodesEven.java
@@ -0,0 +1,111 @@
+package com.tree;
+
+import java.util.*;
+
+/*
+2508. Add Edges to Make Degrees of All Nodes Even
+
+There is an undirected graph consisting of n nodes numbered from 1 to n. You are given the integer n and a 2D
+array edges where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi.
+The graph can be disconnected.
+
+You can add at most two additional edges (possibly none) to this graph so that there are no repeated
+edges and no self-loops.
+
+Return true if it is possible to make the degree of each node in the graph even, otherwise return false.
+
+The degree of a node is the number of edges connected to it.
+
+Example 1:
+Input: n = 5, edges = [[1,2],[2,3],[3,4],[4,2],[1,4],[2,5]]
+Output: true
+Explanation: The above diagram shows a valid way of adding an edge.
+Every node in the resulting graph is connected to an even number of edges.
+
+TC : o(n)
+SC: o(n)
+ */
+public class AddEdgesToMakeDegreesOfAllNodesEven {
+
+    Map<Integer, Set<Integer>> adj;
+
+    public static void main(String[] args) {
+        List<List<Integer>> edges = new ArrayList<>();
+        edges.add(Arrays.asList(1,2));
+        edges.add(Arrays.asList(2,3));
+        edges.add(Arrays.asList(3,4));
+        edges.add(Arrays.asList(4,2));
+        edges.add(Arrays.asList(1,4));
+        edges.add(Arrays.asList(2,5));
+
+        System.out.println(new AddEdgesToMakeDegreesOfAllNodesEven().isPossible(5,edges));
+    }
+
+    public boolean isPossible(int n, List<List<Integer>> edges) {
+        adj = new HashMap<>();
+        // Initialise adjacency List
+        for(int i=0;i<=n;i++){
+            adj.put(i,new HashSet<Integer>());
+        }
+
+        // add the mapping in adjacency list
+        for(List<Integer> edge : edges){
+            adj.get(edge.get(0)).add(edge.get(1));
+            adj.get(edge.get(1)).add(edge.get(0));
+        }
+
+        //get Odd degree node list
+        List<Integer> oddList = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(adj.get(i).size()%2!=0)
+                oddList.add(i);
+        }
+
+        //check if there exist any odd degree node
+        if(oddList.size()==0)
+            return true;
+
+        // handle count(odd degree node) =2
+        if(oddList.size()==2){
+            /*
+            There are two cases:
+            1. both odd degree nodes are connected
+            2. both odd degree are not connected
+            */
+
+            int a = oddList.get(0);
+            int b = oddList.get(1);
+            for(int i=1;i<=n;i++){
+                if(!isConnected(a,i) && !isConnected(b,i)){
+                    return true;
+                }
+            }
+        }
+
+        // handle count(odd degree node) =4
+        if(oddList.size()==4){
+            /*
+            There are four cases:
+            In case if we have 4 odds a,b,c,d
+            option1: We can link (a,b) and (c,d)
+            option2: We can link (a,c) and (b,d)
+            option3: We can link (a,d) and (b,c)
+            otherwise return false.
+            */
+            int a = oddList.get(0);
+            int b = oddList.get(1);
+            int c = oddList.get(2);
+            int d = oddList.get(3);
+            if((!isConnected(a,b) && !isConnected(c,d))||
+                    (!isConnected(a,c) && !isConnected(b,d)) ||
+                    (!isConnected(a,d) && !isConnected(b,c)))
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean isConnected(int a , int b){
+        return adj.get(a).contains(b);
+    }
+}
