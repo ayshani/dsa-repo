b7bec4bc882278de19e11bc09663bfda54d07b64
AYSHANI MALIK <ayshanim@gmail.com>
2023-03-29 17:24:05 +0530

    Update FindMedianFromDataStream.java

--- dsa-repo/src/com/heap/max/FindMedianFromDataStream.java
+++ dsa-repo/src/com/heap/max/FindMedianFromDataStream.java
@@ -16,7 +16,6 @@ MedianFinder() initializes the MedianFinder object.
 void addNum(int num) adds the integer num from the data stream to the data structure.
 double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
 
-
 Example 1:
 
 Input
