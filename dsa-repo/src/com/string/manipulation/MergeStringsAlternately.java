ebe7d19548e986b603b4989b7a14367f87911a74
Ayshani Malik <ayshanim@gmail.com>
2023-04-18 08:30:52 +0530

    Add

---
+++ dsa-repo/src/com/string/manipulation/MergeStringsAlternately.java
@@ -0,0 +1,45 @@
+package com.string.manipulation;
+/*
+1768. Merge Strings Alternately
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order,
+starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+TC : o(n)
+SC: o(1)
+ */
+public class MergeStringsAlternately {
+
+    public static void main(String[] args) {
+        System.out.println(new MergeStringsAlternately().mergeAlternately("abc","pqr"));
+    }
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i=0,j=0;
+        while(i<word1.length() && j<word2.length()){
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
+        }
+
+        if(i<word1.length()){
+            sb.append(word1.substring(i));
+        }
+
+        if(j<word2.length()){
+            sb.append(word2.substring(j++));
+        }
+
+        return sb.toString();
+    }
+}
--- dsa-repo/src/com/string/manipulation/Test.java
+++ dsa-repo/src/com/string/manipulation/Test.java
@@ -1,29 +1,28 @@
 package com.string.manipulation;
 
-public class Test extends Casec implements i{
-    public static void main(String[] args) {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) throws ParseException {
+        Test obj = new Test();
+        obj.parseDate("Mon Dec 28 15:18:16 2021");
+    }
+
+    public void parseDate(String stringDate) throws ParseException {
+        SimpleDateFormat formatRecived = new SimpleDateFormat("E MMM dd HH:mm:ss yyyy");
         try{
-            Float f1 = new Float("3.0");
-            int x = f1.intValue();
-            byte b = f1.byteValue();
-            double d = f1.doubleValue();
-            System.out.println(x+b+d);
-        } catch (NumberFormatException e){
-            System.out.println("bad number");
+            Date date = (Date)formatRecived.parse(stringDate);
+            System.out.println(date);
+            SimpleDateFormat newFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+            String finalString = newFormat.format(date);
+            System.out.println(finalString);
+        }catch (ParseException parseException){
+            System.out.println(parseException.getMessage());
         }
+
     }
 }
 
-interface i{
-    void method();
-}
 
-class Casec{
-    public void method(){
-        System.out.println("Test Casec");
-    }
-}
-
-class ImplC extends Casec implements i{
-
-}
