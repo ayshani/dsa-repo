0bca6a5038f7e2a3845584b13ccf6a9722568281
Ayshani Malik <ayshanim@gmail.com>
2023-05-30 08:40:50 +0530

    Add

---
+++ dsa-repo/src/com/design/DesignHashSet.java
@@ -0,0 +1,65 @@
+package com.design;
+/*
+705. Design HashSet
+
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+
+Example 1:
+
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+ */
+public class DesignHashSet {
+    public static void main(String[] args) {
+        MyHashSet myHashSet = new MyHashSet();
+        myHashSet.add(1);      // set = [1]
+        myHashSet.add(2);      // set = [1, 2]
+        System.out.println(myHashSet.contains(1));
+        System.out.println(myHashSet.contains(3));
+        myHashSet.add(2);      // set = [1, 2]
+        System.out.println(myHashSet.contains(2));
+        myHashSet.remove(2);
+        System.out.println(myHashSet.contains(2));
+    }
+}
+
+class MyHashSet {
+    boolean[] flag;
+    public MyHashSet() {
+        int size = (int)Math.pow(10,6) + 1;
+        flag = new boolean[size];
+    }
+
+    public void add(int key) {
+        flag[key] = true;
+    }
+
+    public void remove(int key) {
+        flag[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return flag[key]== true;
+    }
+}
