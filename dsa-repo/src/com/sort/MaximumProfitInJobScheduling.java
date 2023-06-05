916b450725b7429551f45dbb56aada9a59ba6096
Ayshani Malik <ayshanim@gmail.com>
2023-06-05 22:09:01 +0530

    Add

---
+++ dsa-repo/src/com/sort/MaximumProfitInJobScheduling.java
@@ -0,0 +1,54 @@
+package com.sort;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+/*
+1235. Maximum Profit in Job Scheduling
+
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit
+of profit[i].
+
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there
+are no two jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+
+Example 1:
+Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+Output: 120
+Explanation: The subset chosen is the first and fourth job.
+Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+
+TC: o(nlogn)
+SC: o(n)
+ */
+public class MaximumProfitInJobScheduling {
+
+    public static void main(String[] args) {
+        int[] start = new int[]{1,2,3,3}, end = new int[]{3,4,5,6}, profit = new int[]{50,10,40,70};
+        System.out.println(new MaximumProfitInJobScheduling().jobScheduling(start,end,profit));
+    }
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = profit.length;
+        Integer[][] jobs = new Integer[n][3];
+        for(int i=0;i<n;i++){
+            jobs[i] = new Integer[]{startTime[i],endTime[i],profit[i]};
+        }
+        Arrays.sort(jobs, (a, b)-> a[1]-b[1]);
+
+        TreeMap<Integer,Integer> dp = new TreeMap<>();
+
+        for(Integer[] job : jobs){
+            Map.Entry<Integer,Integer> prevMaxProfitEntry = dp.floorEntry(job[0]);
+            Integer currentProfit = job[2] + (prevMaxProfitEntry== null ? 0 : prevMaxProfitEntry.getValue() );
+
+            if(dp.lastEntry()==null || currentProfit>dp.lastEntry().getValue()){
+                dp.put(job[1],currentProfit);
+            }
+        }
+
+        return dp.lastEntry().getValue();
+    }
+}
