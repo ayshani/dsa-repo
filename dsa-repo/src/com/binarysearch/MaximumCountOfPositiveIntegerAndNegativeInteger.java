fa06451982f7c3a4d2d04b9cb6958bdd34e6e601
Ayshani Malik <ayshanim@gmail.com>
2023-06-08 06:40:43 +0530

    Add

---
+++ dsa-repo/src/com/binarysearch/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -0,0 +1,41 @@
+package com.binarysearch;
+/*
+2529. Maximum Count of Positive Integer and Negative Integer
+
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers
+and the number of negative integers.
+
+In other words, if the number of positive integers in nums is pos and the number of negative integers is neg,
+then return the maximum of pos and neg.
+Note that 0 is neither positive nor negative.
+
+
+
+Example 1:
+
+Input: nums = [-2,-1,-1,1,2,3]
+Output: 3
+Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
+ */
+public class MaximumCountOfPositiveIntegerAndNegativeInteger {
+
+    public int maximumCount(int[] nums) {
+        int negative = binarySearch(nums, 0);
+        int positive = nums.length - binarySearch(nums, 1);
+
+        return Math.max(negative, positive);
+    }
+
+    private int binarySearch(int[] nums, int target){
+        int low =0, high = nums.length-1;
+        while(low<=high){
+            int mid = low +(high - low)/2;
+            if(nums[mid]<target){
+                low = mid+1;
+            } else{
+                high = mid -1;
+            }
+        }
+        return low;
+    }
+}
