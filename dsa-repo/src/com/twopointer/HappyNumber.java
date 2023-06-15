a546e6befd31005ec6364909155ac707b88aa920
Ayshani Malik <ayshanim@gmail.com>
2023-06-15 14:38:36 +0530

    Add

--- dsa-repo/src/com/twopointer/HappyNumber.java
+++ dsa-repo/src/com/twopointer/HappyNumber.java
@@ -1,4 +1,4 @@
-package com.math;
+package com.twopointer;
 /*
 202. Happy Number
 
@@ -7,8 +7,8 @@ Write an algorithm to determine if a number n is happy.
 A happy number is a number defined by the following process:
 
 Starting with any positive integer, replace the number by the sum of the squares of its digits.
-Repeat the process until the number equals 1 (where it will stay),
-or it loops endlessly in a cycle which does not include 1.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which
+does not include 1.
 Those numbers for which this process ends in 1 are happy.
 Return true if n is a happy number, and false if not.
 
@@ -30,17 +30,15 @@ public class HappyNumber {
     public static void main(String[] args) {
         System.out.println(new HappyNumber().isHappy(19));
     }
-
     public boolean isHappy(int n) {
-
-        int slowRunner =n , fastRunner = getNext(n);
+        int slowRUnner = n, fastRunner = getNext(n);
 
         // check if final digit sum becomes 1 or
         //slow and fast doesnt become same. Incase if both becomes same
         // that means there is a cycle exist . If cycle exists
         // then there is no way we can get 1 .
-        while(fastRunner!=1 && slowRunner!=fastRunner){
-            slowRunner = getNext(slowRunner);
+        while(fastRunner != 1 && slowRUnner != fastRunner){
+            slowRUnner = getNext(slowRUnner);
             fastRunner = getNext(getNext(fastRunner));
         }
 
@@ -50,14 +48,15 @@ public class HappyNumber {
     /*
     Get total sum by adding square of digits
      */
-    private int getNext(int n){
-        int totalSum=0;
+    private int getNext(int n) {
+        int num =0;
+
         while(n>0){
             int d = n%10;
-            n/=10;
-            totalSum+= d*d;
+            n /= 10;
+            num += d*d;
         }
-
-        return totalSum;
+        return num;
     }
 }
+
