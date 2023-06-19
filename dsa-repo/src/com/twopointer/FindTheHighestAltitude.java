a3bcde869c01a7300cee416c84ea2e8ac96424ae
Ayshani Malik <ayshanim@gmail.com>
2023-06-19 08:55:28 +0530

    Add

---
+++ dsa-repo/src/com/twopointer/FindTheHighestAltitude.java
@@ -0,0 +1,39 @@
+package com.twopointer;
+/*
+1732. Find the Highest Altitude
+
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between
+points i and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+Example 1:
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+TC : o(n)
+SC: o(1)
+ */
+public class FindTheHighestAltitude {
+
+    public static void main(String[] args) {
+        int[] gains = new int[]{-4,-3,-2,-1,4,3,2};
+        System.out.println(new FindTheHighestAltitude().largestAltitude(gains));
+    }
+    public int largestAltitude(int[] gain) {
+        int n = gain.length;
+        if(n==0)
+            return 0;
+        int j=1, maxAltitude = 0 ,currentAltitude = 0;
+        for(int i=0;i<n;i++){
+            currentAltitude = currentAltitude + gain[i];
+            maxAltitude = Math.max(maxAltitude,currentAltitude);
+            j++;
+        }
+
+        return maxAltitude;
+    }
+}
