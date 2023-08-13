2b412063eca2d582fd3639593a87f7abfdedd1b1
Ayshani Malik <ayshanim@gmail.com>
2023-08-13 19:46:42 +0530

    commit

--- dsa-repo/src/com/dp/CheckIfThereIsAValidPartitionForTheArray.java
+++ dsa-repo/src/com/dp/CheckIfThereIsAValidPartitionForTheArray.java
@@ -1,2 +1,66 @@
-package com.dp;public class CheckIfThereIsAValidPartitionForTheArray {
+package com.dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+2369. Check if There is a Valid Partition For The Array
+
+You are given a 0-indexed integer array nums. You have to partition the array into one or more contiguous subarrays.
+
+We call a partition of the array valid if each of the obtained subarrays satisfies one of the following conditions:
+
+The subarray consists of exactly 2 equal elements. For example, the subarray [2,2] is good.
+The subarray consists of exactly 3 equal elements. For example, the subarray [4,4,4] is good.
+The subarray consists of exactly 3 consecutive increasing elements, that is, the difference between adjacent
+elements is 1. For example, the subarray [3,4,5] is good, but the subarray [1,3,5] is not.
+Return true if the array has at least one valid partition. Otherwise, return false.
+
+
+
+Example 1:
+
+Input: nums = [4,4,4,5,6]
+Output: true
+Explanation: The array can be partitioned into the subarrays [4,4] and [4,5,6].
+This partition is valid, so we return true.
+
+TC : o(n)
+SC: o(n)
+ */
+public class CheckIfThereIsAValidPartitionForTheArray {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{4,4,4,5,6};
+        System.out.println(new CheckIfThereIsAValidPartitionForTheArray().validPartition(nums));
+    }
+    Map<Integer, Boolean> memo = new HashMap<>();
+
+    // Determine if the prefix array nums[0 ~ i] has a valid partition
+    boolean prefixIsValid(int[] nums, int i) {
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+        boolean ans = false;
+
+        // Check 3 possibilities
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            ans |= prefixIsValid(nums, i - 2);
+        }
+        if (i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) {
+            ans |= prefixIsValid(nums, i - 3);
+        }
+        if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i - 1] == nums[i - 2] + 1) {
+            ans |= prefixIsValid(nums, i - 3);
+        }
+        memo.put(i, ans);
+        return ans;
+    }
+
+    public boolean validPartition(int[] nums) {
+        int n = nums.length;
+        memo.put(-1, true);
+
+        return prefixIsValid(nums, n - 1);
+    }
 }
--- dsa-repo/src/com/stack/Pattern132.java
+++ dsa-repo/src/com/stack/Pattern132.java
@@ -1,2 +1,42 @@
-package com.stack;public class Pattern132 {
+package com.stack;
+
+import java.util.Stack;
+
+/*
+132 Pattern
+
+Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and
+nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+ */
+public class Pattern132 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,1,4,2};
+        System.out.println(new Pattern132().find132pattern(nums));
+    }
+
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int third = Integer.MIN_VALUE;
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<third)
+                return true;
+
+            if(!st.isEmpty() && st.peek()<nums[i])
+                third = st.pop();
+
+            st.push(nums[i]);
+        }
+        return false;
+    }
 }
