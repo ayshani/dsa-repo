f783afa4b0d5a795aa941ecf69f9e9c0761e8f35
Ayshani Malik <ayshanim@gmail.com>
2023-09-23 09:57:35 +0530

    commit

--- dsa-repo/src/com/string/manipulation/LongestStringChain.java
+++ dsa-repo/src/com/string/manipulation/LongestStringChain.java
@@ -16,8 +16,6 @@ A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, wher
 
 Return the length of the longest possible word chain with words chosen from the given list of words.
 
-
-
 Example 1:
 
 Input: words = ["a","b","ba","bca","bda","bdca"]
