2982cfd0f0a6d41b5dbbe9130004e48dfe09d4c8
Ayshani Malik <ayshanim@gmail.com>
2023-10-28 09:33:56 +0530

    commit

--- dsa-repo/src/com/dp/CountVowelsPermutation.java
+++ dsa-repo/src/com/dp/CountVowelsPermutation.java
@@ -50,7 +50,6 @@ public class CountVowelsPermutation {
         for(int i=0;i<5;i++){
             dp[1][i]=1;
         }
-
         for(int i=1;i<n;i++){
             dp[i+1][0] = (dp[i][1] + dp[i][2] + dp[i][4])%MOD;
             dp[i+1][1] = (dp[i][0]+dp[i][2])%MOD;
@@ -58,7 +57,6 @@ public class CountVowelsPermutation {
             dp[i+1][3] = (dp[i][2])% MOD;
             dp[i+1][4] = (dp[i][2] + dp[i][3]) % MOD;
         }
-
         long sum =0;
 
         for(int i=0;i<5;i++){
