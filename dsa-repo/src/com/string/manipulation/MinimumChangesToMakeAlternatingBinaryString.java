25f99b60ccefe6a3075f2133a4e1e473dc8a6381
Ayshani Malik <ayshanim@gmail.com>
2023-12-24 09:03:25 +0530

    commit

---
+++ dsa-repo/src/com/string/manipulation/MinimumChangesToMakeAlternatingBinaryString.java
@@ -0,0 +1,43 @@
+package com.string.manipulation;
+/*
+1758. Minimum Changes To Make Alternating Binary String
+
+You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0'
+to '1' or vice versa.
+
+The string is called alternating if no two adjacent characters are equal. For example, the string "010" is
+alternating, while the string "0100" is not.
+
+Return the minimum number of operations needed to make s alternating.
+
+
+
+Example 1:
+
+Input: s = "0100"
+Output: 1
+Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+
+TC : o(n)
+SC: o(1)
+ */
+public class MinimumChangesToMakeAlternatingBinaryString {
+
+    public static void main(String[] args) {
+        System.out.println(new MinimumChangesToMakeAlternatingBinaryString()
+                .minOperations("01000010"));
+    }
+    public int minOperations(String s) {
+        int startWith0 =0;
+        for(int i=0;i<s.length();i++){
+            if(i%2==0){
+                if(s.charAt(i)=='1')
+                    startWith0++;
+            } else{
+                if(s.charAt(i)=='0')
+                    startWith0++;
+            }
+        }
+        return Math.min(startWith0, s.length()-startWith0);
+    }
+}
