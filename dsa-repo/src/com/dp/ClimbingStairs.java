b6b709be8b3f227ea8782e91c7ca8b9c9ea130a4
Ayshani Malik <ayshanim@gmail.com>
2024-01-18 06:31:01 +0530

    commit

--- dsa-repo/src/com/dp/ClimbingStairs.java
+++ dsa-repo/src/com/dp/ClimbingStairs.java
@@ -6,8 +6,6 @@ You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
-
-
 Example 1:
 
 Input: n = 2
@@ -31,6 +29,7 @@ public class ClimbingStairs {
         for(int i=2;i<=n;i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
+
         return dp[n];
     }
 }
