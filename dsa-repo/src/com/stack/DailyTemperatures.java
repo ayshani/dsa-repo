a5b77bbe6e076386811dc3fc9091d606b8aa380f
Ayshani Malik <ayshanim@gmail.com>
2024-01-31 16:41:55 +0530

    commit

--- dsa-repo/src/com/stack/DailyTemperatures.java
+++ dsa-repo/src/com/stack/DailyTemperatures.java
@@ -10,8 +10,6 @@ Given an array of integers temperatures represents the daily temperatures, retur
 that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
 If there is no future day for which this is possible, keep answer[i] == 0 instead.
 
-
-
 Example 1:
 
 Input: temperatures = [73,74,75,71,69,72,76,73]
