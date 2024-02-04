686c371090db20e4dbb17cf63ff6b3eca1507a44
Ayshani Malik <ayshanim@gmail.com>
2024-02-04 06:34:03 +0530

    commit

--- dsa-repo/src/com/slidingwindow/MinimumWindowSubstring.java
+++ dsa-repo/src/com/slidingwindow/MinimumWindowSubstring.java
@@ -32,27 +32,21 @@ public class MinimumWindowSubstring {
     public String minWindow(String s, String t) {
         if(s.length()==0 || s.length()<t.length())
             return "";
-
         String ans ="";
-
         Map<Character,Integer> countMap = new HashMap<>();
         for(char c : t.toCharArray()){
             countMap.put(c,countMap.getOrDefault(c,0)+1);
         }
-
         int start =0, end =0, lengthOfWord = Integer.MAX_VALUE, counter = countMap.size();
 
         while(end<s.length()){
 
             char endCharacter = s.charAt(end);
-
             if(countMap.containsKey(endCharacter)){
                 countMap.put(endCharacter, countMap.getOrDefault(endCharacter,0)-1);
-
                 if(countMap.get(endCharacter) == 0)
                     counter--;
             }
-
             end++;
 
             while(counter == 0){
