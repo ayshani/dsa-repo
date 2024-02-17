508bc0ff394e86ac252a7fc9df2a88d48f914122
Ayshani Malik <ayshanim@gmail.com>
2024-02-17 06:57:18 +0530

    commit

---
+++ dsa-repo/src/com/heap/min/FurthestBuildingYouCanReach.java
@@ -0,0 +1,59 @@
+package com.heap.min;
+
+import java.util.PriorityQueue;
+/*
+1642. Furthest Building You Can Reach
+
+You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
+
+You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
+
+While moving from building i to building i+1 (0-indexed),
+
+If the current building's height is greater than or equal to the next building's height, you do not need a ladder
+or bricks.
+If the current building's height is less than the next building's height, you can either use one ladder or
+(h[i+1] - h[i]) bricks.
+Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.
+
+
+
+Example 1:
+Input: heights = [4,2,7,6,9,14,12], bricks = 5, ladders = 1
+Output: 4
+Explanation: Starting at building 0, you can follow these steps:
+- Go to building 1 without using ladders nor bricks since 4 >= 2.
+- Go to building 2 using 5 bricks. You must use either bricks or ladders because 2 < 7.
+- Go to building 3 without using ladders nor bricks since 7 >= 6.
+- Go to building 4 using your only ladder. You must use either bricks or ladders because 6 < 9.
+It is impossible to go beyond building 4 because you do not have any more bricks or ladders.
+
+TC : o(nlogn)
+SC: o(n)
+ */
+public class FurthestBuildingYouCanReach {
+
+    public static void main(String[] args) {
+        System.out.println(new FurthestBuildingYouCanReach()
+                .furthestBuilding(new int[]{4,12,2,7,3,18,20,3,19}, 10,2));
+    }
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        int n = heights.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<n-1;i++){
+            int diff = heights[i+1] - heights[i];
+            if(diff>0){
+                pq.add(diff);
+            }
+
+            if(pq.size()>ladders){
+                bricks -= pq.poll();
+            }
+
+            if(bricks<0){
+                return i;
+            }
+        }
+        return n-1;
+    }
+}
---
+++ dsa-repo/src/com/tree/DeleteNodesAndReturnForest.java
@@ -0,0 +1,50 @@
+package com.tree;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+1110. Delete Nodes And Return Forest
+
+Given the root of a binary tree, each node in the tree has a distinct value.
+
+After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+
+Return the roots of the trees in the remaining forest. You may return the result in any order.
+
+
+
+Example 1:
+Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+
+ */
+public class DeleteNodesAndReturnForest {
+
+    List<TreeNode> nodeList;
+    Set<Integer> deleteSet;
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        nodeList = new ArrayList<>();
+        deleteSet = new HashSet<>();
+        for(int num : to_delete){
+            deleteSet.add(num);
+        }
+        util(root, true);
+        return nodeList;
+    }
+
+    private TreeNode util(TreeNode root, boolean isRoot){
+        if(root == null){
+            return null;
+        }
+        boolean deleted = deleteSet.contains(root.val);
+        if(isRoot && !deleted){
+            nodeList.add(root);
+        }
+        root.left = util(root.left, deleted);
+        root.right = util(root.right, deleted);
+        return deleted ? null : root;
+    }
+}
