05d57797041a44d81b7b1ea5b5eb603eab3e0db4
Ayshani Malik <ayshanim@gmail.com>
2024-03-07 06:37:03 +0530

    commit

--- dsa-repo/src/com/linkedlist/LinkedListRandomNode.java
+++ dsa-repo/src/com/linkedlist/LinkedListRandomNode.java
@@ -7,12 +7,14 @@ import java.util.Random;
 /*
 382. Linked List Random Node
 
-Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same
+probability of being chosen.
 
 Implement the Solution class:
 
 Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
-int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be
+equally likely to be chosen.
 
 Example 1:
 Input
--- dsa-repo/src/com/linkedlist/MiddleOfTheLinkedList.java
+++ dsa-repo/src/com/linkedlist/MiddleOfTheLinkedList.java
@@ -33,13 +33,10 @@ public class MiddleOfTheLinkedList {
     }
     public ListNode middleNode(ListNode head) {
         ListNode slow = head, fast = head;
-
-
         while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow = slow.next;
         }
-
         return slow;
     }
 }
