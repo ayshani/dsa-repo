421de8c6762260e72a4f67d254757422465eb45c
Ayshani Malik <ayshanim@gmail.com>
2024-03-11 06:57:51 +0530

    commit

--- dsa-repo/src/com/sort/SortTheStudentsByTheirKthScore.java
+++ dsa-repo/src/com/sort/SortTheStudentsByTheirKthScore.java
@@ -44,8 +44,6 @@ public class SortTheStudentsByTheirKthScore {
         );
     }
     public int[][] sortTheStudents(int[][] score, int k) {
-        int m = score.length, n = score[0].length;
-
         Arrays.sort(score, (a, b) -> b[k]- a[k]);
         return score;
     }
