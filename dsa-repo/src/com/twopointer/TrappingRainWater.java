535e1e98f3555d63c5461a6329b06994c5911e4a
Ayshani Malik <ayshanim@gmail.com>
2024-04-12 15:02:45 +0530

    commit

--- dsa-repo/src/com/twopointer/TrappingRainWater.java
+++ dsa-repo/src/com/twopointer/TrappingRainWater.java
@@ -32,6 +32,7 @@ public class TrappingRainWater {
         int left =0, right = height.length-1;
         int ans = 0;
         int leftMax =0, rightMax =0;
+
         while(left<right){
             if(height[left]<height[right]){
                 if(height[left]>=leftMax)
