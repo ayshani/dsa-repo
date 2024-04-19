f5e5f9676f1dda71abac2f661b20e73be54d0611
Ayshani Malik <ayshanim@gmail.com>
2024-04-19 07:07:26 +0530

    commit

--- dsa-repo/src/com/graph/dfs/NumberOfIslands.java
+++ dsa-repo/src/com/graph/dfs/NumberOfIslands.java
@@ -1,6 +1,9 @@
 package com.graph.dfs;
 /*
-Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+200. Number of Islands
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of
+islands.
 
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may
 assume all four edges of the grid are all surrounded by water.
@@ -44,7 +47,6 @@ public class NumberOfIslands {
                 }
             }
         }
-
         return count;
     }
 
