6c390e7d48a6de3db4a2121a4b54bab4bf41dc60
Ayshani Malik <ayshanim@gmail.com>
2024-04-24 07:09:47 +0530

    commit

--- dsa-repo/src/com/dp/NthTribonacciNumber.java
+++ dsa-repo/src/com/dp/NthTribonacciNumber.java
@@ -8,8 +8,6 @@ T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 
 Given n, return the value of Tn.
 
-
-
 Example 1:
 
 Input: n = 4
@@ -36,7 +34,6 @@ public class NthTribonacciNumber {
         for(int i=3;i<=n;i++){
             dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
-
         return dp[n];
     }
 }
