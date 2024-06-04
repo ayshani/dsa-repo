3a20fa4854266a239b96bfc17bd1727e743efb86
Ayshani Malik <ayshanim@gmail.com>
2024-06-04 08:39:23 +0530

    commit

--- dsa-repo/src/com/string/manipulation/LongestPalindrome.java
+++ dsa-repo/src/com/string/manipulation/LongestPalindrome.java
@@ -52,14 +52,12 @@ public class LongestPalindrome {
         for(char c: s.toCharArray()){
             count[c]++;
         }
-
         int ans =0;
         for(int v : count){
             ans+=v/2*2;
             if(ans%2==0 && v%2==1)
                 ans++;
         }
-
         return ans;
     }
 
