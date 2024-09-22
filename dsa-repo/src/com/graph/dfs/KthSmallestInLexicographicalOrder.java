20fa116b863a25cb69030f2072557c15ef197455
Ayshani Malik <ayshanim@gmail.com>
2024-09-22 10:23:47 +0530

    initial commit

---
+++ dsa-repo/src/com/graph/dfs/KthSmallestInLexicographicalOrder.java
@@ -0,0 +1,54 @@
+package com.graph.dfs;
+/*
+440. K-th Smallest in Lexicographical Order
+
+Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+
+
+
+Example 1:
+
+Input: n = 13, k = 2
+Output: 10
+Explanation: The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest
+number is 10.
+
+TC : o((logn)^2)
+SC: o(logn)
+ */
+public class KthSmallestInLexicographicalOrder {
+
+    public static void main(String[] args) {
+        System.out.println(new KthSmallestInLexicographicalOrder().findKthNumber(13, 2));
+    }
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+
+        while (k > 0) {
+            int step = countSteps(n, curr, curr + 1);
+            // If the steps are less than or equal to k, we skip this prefix's subtree
+            if (step <= k) {
+                // Move to the next prefix and decrease k by the number of steps we skip
+                curr++;
+                k -= step;
+            } else {
+                // Move to the next level of the tree and decrement k by 1
+                curr *= 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+
+    // To count how many numbers exist between prefix1 and prefix2
+    private int countSteps(int n, long prefix1, long prefix2) {
+        int steps = 0;
+        while (prefix1 <= n) {
+            steps += Math.min(n + 1, prefix2) - prefix1;
+            prefix1 *= 10;
+            prefix2 *= 10;
+        }
+        return steps;
+    }
+}
