ffe5690ba4ca58749f6777f9e8aed996ddb59afc
Ayshani Malik <ayshanim@gmail.com>
2024-10-30 06:42:37 +0530

    initial commit

--- dsa-repo/src/com/dp/MinimumNumberOfRemovalsToMakeMountainArray.java
+++ dsa-repo/src/com/dp/MinimumNumberOfRemovalsToMakeMountainArray.java
@@ -1,4 +1,7 @@
 package com.dp;
+
+import java.util.Arrays;
+
 /*
 1671. Minimum Number of Removals to Make Mountain Array
 
@@ -31,6 +34,7 @@ public class MinimumNumberOfRemovalsToMakeMountainArray {
     public static void main(String[] args) {
         int[] nums = new int[]{2,1,1,5,6,2,3,1};
         System.out.println(new MinimumNumberOfRemovalsToMakeMountainArray().minimumMountainRemovals(nums));
+        System.out.println(new MinimumNumberOfRemovalsToMakeMountainArray().minimumMountainRemovalsV2(nums));
     }
     public int minimumMountainRemovals(int[] nums) {
         int n = nums.length;
@@ -72,5 +76,45 @@ public class MinimumNumberOfRemovalsToMakeMountainArray {
             }
         }
         return dp;
+    }
+
+    public int minimumMountainRemovalsV2(int[] nums) {
+        int N = nums.length;
+
+        int[] lisLength = new int[N];
+        int[] ldsLength = new int[N];
+
+        Arrays.fill(lisLength, 1);
+        Arrays.fill(ldsLength, 1);
+
+        // Stores the length of longest increasing subsequence that ends at i.
+        for (int i = 0; i < N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    lisLength[i] = Math.max(lisLength[i], lisLength[j] + 1);
+                }
+            }
+        }
+
+        // Stores the length of longest decreasing subsequence that starts at i.
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = i + 1; j < N; j++) {
+                if (nums[i] > nums[j]) {
+                    ldsLength[i] = Math.max(ldsLength[i], ldsLength[j] + 1);
+                }
+            }
+        }
+
+        int minRemovals = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            if (lisLength[i] > 1 && ldsLength[i] > 1) {
+                minRemovals = Math.min(
+                        minRemovals,
+                        N - lisLength[i] - ldsLength[i] + 1
+                );
+            }
+        }
+
+        return minRemovals;
     }
 }
