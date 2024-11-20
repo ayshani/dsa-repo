aa1be7ac6ef0f683f6da37878efb83ca0fae34a3
Ayshani Malik <ayshanim@gmail.com>
2024-11-20 18:47:23 +0530

    initial commit

--- dsa-repo/src/com/slidingwindow/TakeKOfEachCharacterFromLeftAndRight.java
+++ dsa-repo/src/com/slidingwindow/TakeKOfEachCharacterFromLeftAndRight.java
@@ -2,9 +2,11 @@ package com.slidingwindow;
 /*
 2516. Take K of Each Character From Left and Right
 
-You are given a string s consisting of the characters 'a', 'b', and 'c' and a non-negative integer k. Each minute, you may take either the leftmost character of s, or the rightmost character of s.
+You are given a string s consisting of the characters 'a', 'b', and 'c' and a non-negative integer k. Each minute,
+you may take either the leftmost character of s, or the rightmost character of s.
 
-Return the minimum number of minutes needed for you to take at least k of each character, or return -1 if it is not possible to take k of each character.
+Return the minimum number of minutes needed for you to take at least k of each character, or return -1 if it is
+not possible to take k of each character.
 
 
 
