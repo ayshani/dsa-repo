39e8d8ee0c867f75bb16c22233ed2fddf610cfa3
Ayshani Malik <ayshanim@gmail.com>
2025-03-05 08:48:24 +0530

    initial commit

--- dsa-repo/src/com/brainteaser/CountTotalNumberOfColoredCells.java
+++ dsa-repo/src/com/brainteaser/CountTotalNumberOfColoredCells.java
@@ -24,25 +24,27 @@ Output: 5
 Explanation: After 2 minutes, there are 4 colored cells on the boundary and 1 in the center, so we return 5.
 
 Intuition
-In the previous approach, we iterated n - 1 times and added an increasing multiple of 4 in each step. This resulted in a linear time complexity, which is efficient for moderate values of n but can be avoided with a direct formula. Instead of looping, we want to express the total count of blue cells as a mathematical equation.
+In the previous approach, we iterated n - 1 times and added an increasing multiple of 4 in each step.
+This resulted in a linear time complexity, which is efficient for moderate values of n but can be avoided with
+a direct formula. Instead of looping, we want to express the total count of blue cells as a mathematical equation.
 
-From our earlier observations, we know that we start with a single blue cell and then successively add multiples of 4: first 4 × 1, then 4 × 2, then 4 × 3, and so on, continuing for n - 1 steps. This means the total count follows the sum:
+From our earlier observations, we know that we start with a single blue cell and then successively add multiples
+of 4: first 4 × 1, then 4 × 2, then 4 × 3, and so on, continuing for n - 1 steps. This means the total count
+follows the sum:
 
 1+(4×1)+(4×2)+...+(4×(n−1))
 
-Recognizing that the sum inside the parentheses is simply the arithmetic series 1+2+...+(n−1), we use the formula for the sum of the first m natural numbers:
+Recognizing that the sum inside the parentheses is simply the arithmetic series 1+2+...+(n−1), we use the formula
+for the sum of the first m natural numbers:
 
-1+4×
-2
-(n−1)×n
-​
-
+1+4×(n−1)×n/2
 
 Expanding and simplifying, we get:
 
 1+2×(n−1)×n
 
-This formula allows us to immediately compute the answer, eliminating the need for iteration. We can now directly return the result in constant time using this equation.
+This formula allows us to immediately compute the answer, eliminating the need for iteration.
+We can now directly return the result in constant time using this equation.
 
 
 
