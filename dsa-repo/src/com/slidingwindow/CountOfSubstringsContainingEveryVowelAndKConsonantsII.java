48965b2ae5eb449d35b3ff83131564b12cd28b7b
Ayshani Malik <ayshanim@gmail.com>
2025-03-10 23:39:14 +0530

    initial commit

---
+++ dsa-repo/src/com/slidingwindow/CountOfSubstringsContainingEveryVowelAndKConsonantsII.java
@@ -0,0 +1,86 @@
+package com.slidingwindow;
+
+import java.util.HashMap;
+
+/*
+3306. Count of Substrings Containing Every Vowel and K Consonants II
+
+You are given a string word and a non-negative integer k.
+
+Return the total number of substrings of word that contain every vowel ('a', 'e', 'i', 'o', and 'u') at least once and exactly k consonants.
+
+
+
+Example 1:
+
+Input: word = "aeioqq", k = 1
+
+Output: 0
+
+Explanation:
+
+There is no substring with every vowel.
+
+TC : o(n)
+SC: o(1)
+ */
+public class CountOfSubstringsContainingEveryVowelAndKConsonantsII {
+
+    public static void main(String[] args) {
+        System.out.println(new CountOfSubstringsContainingEveryVowelAndKConsonantsII().countOfSubstrings("aeioqq",1));
+    }
+    public long countOfSubstrings(String word, int k) {
+        return atLeastK(word, k) - atLeastK(word, k + 1);
+    }
+
+    private long atLeastK(String word, int k) {
+        long numValidSubstrings = 0;
+        int start = 0;
+        int end = 0;
+        // keep track of counts of vowels and consonants
+        HashMap<Character, Integer> vowelCount = new HashMap<>();
+        int consonantCount = 0;
+
+        // start sliding window
+        while (end < word.length()) {
+            // insert new letter
+            char newLetter = word.charAt(end);
+
+            // update counts
+            if (isVowel(newLetter)) {
+                vowelCount.put(
+                        newLetter,
+                        vowelCount.getOrDefault(newLetter, 0) + 1
+                );
+            } else {
+                consonantCount++;
+            }
+
+            // shrink window while we have a valid substring
+            while (vowelCount.size() == 5 && consonantCount >= k) {
+                numValidSubstrings += word.length() - end;
+                char startLetter = word.charAt(start);
+                if (isVowel(startLetter)) {
+                    vowelCount.put(
+                            startLetter,
+                            vowelCount.get(startLetter) - 1
+                    );
+                    if (vowelCount.get(startLetter) == 0) {
+                        vowelCount.remove(startLetter);
+                    }
+                } else {
+                    consonantCount--;
+                }
+                start++;
+            }
+
+            end++;
+        }
+
+        return numValidSubstrings;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
