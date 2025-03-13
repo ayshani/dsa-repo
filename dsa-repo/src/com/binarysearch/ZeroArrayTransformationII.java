b5687b4c4109c469cf6286fd62ce4e6432294e77
Ayshani Malik <ayshanim@gmail.com>
2025-03-13 08:21:12 +0530

    initial commit

---
+++ dsa-repo/src/com/binarysearch/ZeroArrayTransformationII.java
@@ -0,0 +1,80 @@
+package com.binarysearch;
+/*
+3356. Zero Array Transformation II
+
+You are given an integer array nums of length n and a 2D array queries where queries[i] = [li, ri, vali].
+
+Each queries[i] represents the following action on nums:
+
+Decrement the value at each index in the range [li, ri] in nums by at most vali.
+The amount by which each value is decremented can be chosen independently for each index.
+A Zero Array is an array with all its elements equal to 0.
+
+Return the minimum possible non-negative value of k, such that after processing the first k queries in sequence,
+nums becomes a Zero Array. If no such k exists, return -1.
+
+Input: nums = [2,0,2], queries = [[0,2,1],[0,2,1],[1,1,3]]
+
+Output: 2
+
+Explanation:
+
+For i = 0 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+The array will become [1, 0, 1].
+For i = 1 (l = 0, r = 2, val = 1):
+Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+The array will become [0, 0, 0], which is a Zero Array. Therefore, the minimum value of k is 2.
+
+TC : o(logn * (m+n))
+SC : o(n)
+ */
+public class ZeroArrayTransformationII {
+
+    public static void main(String[] args) {
+        System.out.println(new ZeroArrayTransformationII().minZeroArray(new int[]{2,0,2}, new int[][]{
+                {0,2,1},{0,2,1},{1,1,3}
+        }));
+    }
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length, left = 0, right = queries.length;
+
+        // Zero array isn't formed after all queries are processed
+        if (!currentIndexZero(nums, queries, right)) return -1;
+
+        // Binary Search
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (currentIndexZero(nums, queries, middle)) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        // Return earliest query that zero array can be formed
+        return left;
+    }
+
+    private boolean currentIndexZero(int[] nums, int[][] queries, int k) {
+        int n = nums.length, sum = 0;
+        int[] differenceArray = new int[n + 1];
+
+        // Process query
+        for (int queryIndex = 0; queryIndex < k; queryIndex++) {
+            int left = queries[queryIndex][0], right =
+                    queries[queryIndex][1], val = queries[queryIndex][2];
+
+            // Process start and end of range
+            differenceArray[left] += val;
+            differenceArray[right + 1] -= val;
+        }
+
+        // Check if zero array can be formed
+        for (int numIndex = 0; numIndex < n; numIndex++) {
+            sum += differenceArray[numIndex];
+            if (sum < nums[numIndex]) return false;
+        }
+        return true;
+    }
+}
