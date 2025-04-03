ef33f82336c11f9f0bdc913e624f8e1f5bd5d169
Ayshani Malik <ayshanim@gmail.com>
2025-04-03 21:36:44 +0530

    initial commit

---
+++ dsa-repo/src/com/greedy/MaximumValueOfAnOrderedTripletII.java
@@ -0,0 +1,44 @@
+package com.greedy;
+/*
+2874. Maximum Value of an Ordered Triplet II
+
+You are given a 0-indexed integer array nums.
+
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+
+
+Example 1:
+
+Input: nums = [12,6,1,2,7]
+Output: 77
+Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+It can be shown that there are no ordered triplets of indices with a value greater than 77.
+
+TC : o(n)
+SC: o(n)
+ */
+public class MaximumValueOfAnOrderedTripletII {
+
+    public static void main(String[] args) {
+        System.out.println(new MaximumValueOfAnOrderedTripletII().maximumTripletValue(
+                new int[]{12,6,1,2,7}
+        ));
+    }
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], nums[i - 1]);
+            rightMax[n - 1 - i] = Math.max(rightMax[n - i], nums[n - i]);
+        }
+        long res = 0;
+        for (int j = 1; j < n - 1; j++) {
+            res = Math.max(res, (long) (leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return res;
+    }
+}
