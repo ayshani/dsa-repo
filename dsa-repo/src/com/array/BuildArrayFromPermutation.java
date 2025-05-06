8c801d04550da650842d7c3a66105f44f89f50c3
Ayshani Malik <ayshanim@gmail.com>
2025-05-06 07:37:28 +0530

    initial commit

--- dsa-repo/src/com/array/BuildArrayFromPermutation.java
+++ dsa-repo/src/com/array/BuildArrayFromPermutation.java
@@ -32,9 +32,11 @@ public class BuildArrayFromPermutation {
     }
     public int[] buildArray(int[] nums) {
         int[] ar = new int[nums.length];
+
         for(int i=0;i<nums.length;i++){
             ar[i] = nums[nums[i]];
         }
+
         return ar;
     }
 }
