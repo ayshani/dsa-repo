0272e6a9df56e4e0af3463f8ca04a05a01cfd9bc
Ayshani Malik <ayshanim@gmail.com>
2025-05-07 07:46:19 +0530

    initial commit

---
+++ dsa-repo/src/com/graph/shortestpathalgorithm/singlesource/dijkstraalgo/FindMinimumTimeToReachLastRoomI.java
@@ -0,0 +1,94 @@
+package com.graph.shortestpathalgorithm.singlesource.dijkstraalgo;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/*
+3341. Find Minimum Time to Reach Last Room I
+
+There is a dungeon with n x m rooms arranged as a grid.
+
+You are given a 2D array moveTime of size n x m, where moveTime[i][j] represents the minimum time in seconds when you can start moving to that room. You start from the room (0, 0) at time t = 0 and can move to an adjacent room. Moving between adjacent rooms takes exactly one second.
+
+Return the minimum time to reach the room (n - 1, m - 1).
+
+Two rooms are adjacent if they share a common wall, either horizontally or vertically.
+
+
+
+Example 1:
+
+Input: moveTime = [[0,4],[4,4]]
+
+Output: 6
+
+Explanation:
+
+The minimum time required is 6 seconds.
+
+At time t == 4, move from room (0, 0) to room (1, 0) in one second.
+At time t == 5, move from room (1, 0) to room (1, 1) in one second.
+
+TC : o(mnlog(mn))
+SC: o(mn)
+ */
+public class FindMinimumTimeToReachLastRoomI {
+
+    public static void main(String[] args) {
+        System.out.println(new FindMinimumTimeToReachLastRoomI().minTimeToReach(
+                new int[][]{
+                        {0,4},{4,4}
+                }
+        ));
+    }
+    private static final int INF = 0x3f3f3f3f;
+
+    public int minTimeToReach(int[][] moveTime) {
+        int n = moveTime.length, m = moveTime[0].length;
+        int[][] d = new int[n][m];
+        boolean[][] v = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(d[i], INF);
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        d[0][0] = 0;
+        PriorityQueue<State> q = new PriorityQueue<>();
+        q.offer(new State(0,0,0));
+
+        while(!q.isEmpty()){
+            State s = q.poll();
+            if(v[s.x][s.y]){
+                continue;
+            }
+            v[s.x][s.y] = true;
+            for(int[] dir : dirs){
+                int nx = s.x + dir[0];
+                int ny = s.y + dir[1];
+                if(nx<0 || nx >=n || ny<0 || ny>=m){
+                    continue;
+                }
+                int dist = Math.max(d[s.x][s.y] , moveTime[nx][ny]) +1;
+                if(d[nx][ny] > dist){
+                    d[nx][ny] = dist;
+                    q.offer(new State(nx,ny,dist));
+                }
+            }
+        }
+        return d[n-1][m-1];
+    }
+}
+
+class State implements Comparable<State>{
+    int x,y,dist;
+    State(int x, int y, int dist){
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+    }
+
+    @Override
+    public int compareTo(State other){
+        return Integer.compare(this.dist, other.dist);
+    }
+}
