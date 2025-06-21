2df10b48ba896ffb389604ce23475d9e40581218
Ayshani Malik <ayshanim@gmail.com>
2025-06-21 10:21:22 +0530

    initial commit

---
+++ dsa-repo/src/com/map/MinimumDeletionsToMakeStringKSpecial.java
@@ -0,0 +1,54 @@
+package com.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+3085. Minimum Deletions to Make String K-Special
+
+You are given a string word and an integer k.
+
+We consider word to be k-special if |freq(word[i]) - freq(word[j])| <= k for all indices i and j in the string.
+
+Here, freq(x) denotes the frequency of the character x in word, and |y| denotes the absolute value of y.
+
+Return the minimum number of characters you need to delete to make word k-special.
+
+
+
+Example 1:
+
+Input: word = "aabcaba", k = 0
+
+Output: 3
+
+Explanation: We can make word 0-special by deleting 2 occurrences of "a" and 1 occurrence of "c". Therefore, word becomes equal to "baba" where freq('a') == freq('b') == 2.
+
+TC : o(n+ c^2)
+SC: o(c)
+ */
+public class MinimumDeletionsToMakeStringKSpecial {
+
+    public static void main(String[] args) {
+        System.out.println(new MinimumDeletionsToMakeStringKSpecial().minimumDeletions("aabcaba",0));
+    }
+    public int minimumDeletions(String word, int k) {
+        Map<Character, Integer> cnt = new HashMap<>();
+        for (char ch : word.toCharArray()) {
+            cnt.put(ch, cnt.getOrDefault(ch, 0) + 1);
+        }
+        int res = word.length();
+        for (int a : cnt.values()) {
+            int deleted = 0;
+            for (int b : cnt.values()) {
+                if (a > b) {
+                    deleted += b;
+                } else if (b > a + k) {
+                    deleted += b - (a + k);
+                }
+            }
+            res = Math.min(res, deleted);
+        }
+        return res;
+    }
+}
