3f2e50de37b652662c3099efac4951db76e3ab77
Ayshani Malik <ayshanim@gmail.com>
2025-06-27 08:04:46 +0530

    initial commit

---
+++ dsa-repo/src/com/queue/LongestSubsequenceRepeatedKTimes.java
@@ -0,0 +1,88 @@
+package com.queue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+2014. Longest Subsequence Repeated k Times
+
+You are given a string s of length n, and an integer k. You are tasked to find the longest subsequence repeated k times in string s.
+
+A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+A subsequence seq is repeated k times in the string s if seq * k is a subsequence of s, where seq * k represents a string constructed by concatenating seq k times.
+
+For example, "bba" is repeated 2 times in the string "bababcba", because the string "bbabba", constructed by concatenating "bba" 2 times, is a subsequence of the string "bababcba".
+Return the longest subsequence repeated k times in string s. If multiple such subsequences are found, return the lexicographically largest one. If there is no such subsequence, return an empty string.
+
+
+
+Example 1:
+Input: s = "letsleetcode", k = 2
+Output: "let"
+Explanation: There are two longest subsequences repeated 2 times: "let" and "ete".
+"let" is the lexicographically largest one.
+
+Time complexity: O(n⋅⌊k n⌋!).
+Space complexity: O(⌊k n⌋!).
+ */
+public class LongestSubsequenceRepeatedKTimes {
+
+    public static void main(String[] args) {
+        System.out.println(new LongestSubsequenceRepeatedKTimes().longestSubsequenceRepeatedK("letsleetcode", 2));
+    }
+    public String longestSubsequenceRepeatedK(String s, int k) {
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        List<Character> candidate = new ArrayList<>();
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] >= k) {
+                candidate.add((char) ('a' + i));
+            }
+        }
+
+        Queue<String> q = new LinkedList<>();
+        for (char ch : candidate) {
+            q.add(String.valueOf(ch));
+        }
+
+        String ans = "";
+        while (!q.isEmpty()) {
+            String curr = q.poll();
+            if (curr.length() > ans.length()) {
+                ans = curr;
+            }
+            // generate the next candidate string
+            for (char ch : candidate) {
+                String next = curr + ch;
+                if (isKRepeatedSubsequence(s, next, k)) {
+                    q.add(next);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean isKRepeatedSubsequence(String s, String t, int k) {
+        int pos = 0, matched = 0;
+        int m = t.length();
+        for (char ch : s.toCharArray()) {
+            if (ch == t.charAt(pos)) {
+                pos++;
+                if (pos == m) {
+                    pos = 0;
+                    matched++;
+                    if (matched == k) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
