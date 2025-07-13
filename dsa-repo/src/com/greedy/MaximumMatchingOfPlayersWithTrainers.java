49cf5ca731a46d9de209020e9674a5fb6d4402e6
Ayshani Malik <ayshanim@gmail.com>
2025-07-13 10:47:13 +0530

    initial commit

--- dsa-repo/src/com/greedy/MaximumMatchingOfPlayersWithTrainers.java
+++ dsa-repo/src/com/greedy/MaximumMatchingOfPlayersWithTrainers.java
@@ -1,2 +1,60 @@
-package com.greedy;public class MaximumMatchingOfPlayersWithTrainers {
+package com.greedy;
+
+import java.util.Arrays;
+
+/*
+2410. Maximum Matching of Players With Trainers
+
+
+You are given a 0-indexed integer array players, where players[i] represents the ability of the ith player. You are also given a 0-indexed integer array trainers, where trainers[j] represents the training capacity of the jth trainer.
+
+The ith player can match with the jth trainer if the player's ability is less than or equal to the trainer's training capacity. Additionally, the ith player can be matched with at most one trainer, and the jth trainer can be matched with at most one player.
+
+Return the maximum number of matchings between players and trainers that satisfy these conditions.
+
+
+
+Example 1:
+
+Input: players = [4,7,9], trainers = [8,2,5,8]
+Output: 2
+Explanation:
+One of the ways we can form two matchings is as follows:
+- players[0] can be matched with trainers[0] since 4 <= 8.
+- players[1] can be matched with trainers[3] since 7 <= 8.
+It can be proven that 2 is the maximum number of matchings that can be formed.
+
+TC : o(nlogn)
+SC: o(1)
+ */
+public class MaximumMatchingOfPlayersWithTrainers {
+
+    public static void main(String[] args) {
+        System.out.println(new MaximumMatchingOfPlayersWithTrainers().matchPlayersAndTrainers(
+                new int[]{4,7,9}, new int[]{8,2,5,8}
+        ));
+    }
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int m = players.length;
+        int n = trainers.length;
+        int count = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            if (players[i] <= trainers[j]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return count;
+    }
 }
