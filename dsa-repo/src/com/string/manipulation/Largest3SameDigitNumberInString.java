bab94da3232feac0daa3725d344e3875ce53668a
Ayshani Malik <ayshanim@gmail.com>
2025-08-14 08:39:54 +0530

    initial commit

--- dsa-repo/src/com/string/manipulation/Largest3SameDigitNumberInString.java
+++ dsa-repo/src/com/string/manipulation/Largest3SameDigitNumberInString.java
@@ -31,11 +31,13 @@ public class Largest3SameDigitNumberInString {
     }
     public String largestGoodInteger(String num) {
         char maxDigit = '\0';
+
         for(int i=0;i<num.length()-2;i++){
             if(num.charAt(i)== num.charAt(i+1) && num.charAt(i)== num.charAt(i+2)){
                 maxDigit = (char) Math.max(maxDigit, num.charAt(i));
             }
         }
+
         return maxDigit == '\0' ? "" : new String(new char[]{maxDigit,maxDigit, maxDigit}) ;
     }
 }
