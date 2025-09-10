294542ee5eb94aaef12c48692130072d19fb08d1
Ayshani Malik <ayshanim@gmail.com>
2025-09-10 08:14:53 +0530

    initial commit

---
+++ dsa-repo/src/com/greedy/MinimumNumberOfPeopleToTeach.java
@@ -0,0 +1,68 @@
+package com.greedy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/*
+1733. Minimum Number of People to Teach
+
+On a social network consisting of m users and some friendships between users, two users can communicate with each other if they know a common language.
+
+You are given an integer n, an array languages, and an array friendships where:
+
+There are n languages numbered 1 through n,
+languages[i] is the set of languages the ith user knows, and
+friendships[i] = [ui, vi] denotes a friendship between the users ui and vi.
+You can choose one language and teach it to some users so that all friends can communicate with each other. Return the minimum number of users you need to teach.
+
+Note that friendships are not transitive, meaning if x is a friend of y and y is a friend of z, this doesn't guarantee that x is a friend of z.
+
+
+Example 1:
+
+Input: n = 2, languages = [[1],[2],[1,2]], friendships = [[1,2],[1,3],[2,3]]
+Output: 1
+Explanation: You can either teach user 1 the second language or user 2 the first language.
+
+TC : o(m*n)
+SC: o(m+n)
+ */
+public class MinimumNumberOfPeopleToTeach {
+
+    public static void main(String[] args) {
+        System.out.println(new MinimumNumberOfPeopleToTeach().minimumTeachings(
+                2, new int[][]{{1},{2},{1,2}}, new int[][]{{1,2},{1,3},{2,3}}
+        ));
+    }
+    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        Set<Integer> cncon = new HashSet<>();
+        for (int[] friendship : friendships) {
+            Map<Integer, Integer> mp = new HashMap<>();
+            boolean conm = false;
+            for (int lan : languages[friendship[0] - 1]) {
+                mp.put(lan, 1);
+            }
+            for (int lan : languages[friendship[1] - 1]) {
+                if (mp.containsKey(lan)) {
+                    conm = true;
+                    break;
+                }
+            }
+            if (!conm) {
+                cncon.add(friendship[0] - 1);
+                cncon.add(friendship[1] - 1);
+            }
+        }
+        int max_cnt = 0;
+        int[] cnt = new int[n + 1];
+        for (int friendship : cncon) {
+            for (int lan : languages[friendship]) {
+                cnt[lan]++;
+                max_cnt = Math.max(max_cnt, cnt[lan]);
+            }
+        }
+        return cncon.size() - max_cnt;
+    }
+}
