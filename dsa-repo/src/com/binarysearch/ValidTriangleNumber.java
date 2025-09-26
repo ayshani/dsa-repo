abba3c6de7b226a9984327b8ce5ef1cc4f9779f9
Ayshani Malik <ayshanim@gmail.com>
2025-09-26 21:58:44 +0530

    initial commit

---
+++ dsa-repo/src/com/binarysearch/ValidTriangleNumber.java
@@ -0,0 +1,50 @@
+package com.binarysearch;
+
+import java.util.Arrays;
+
+/*
+611. Valid Triangle Number
+
+Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+
+
+Example 1:
+
+Input: nums = [2,2,3,4]
+Output: 3
+Explanation: Valid combinations are:
+2,3,4 (using the first 2)
+2,3,4 (using the second 2)
+2,2,3
+
+TC :
+ */
+public class ValidTriangleNumber {
+
+    public static void main(String[] args) {
+        System.out.println(new ValidTriangleNumber().triangleNumber(new int[]{2,2,3,4}));
+    }
+    int binarySearch(int nums[], int l, int r, int x) {
+        while (r >= l && r < nums.length) {
+            int mid = (l + r) / 2;
+            if (nums[mid] >= x)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+    public int triangleNumber(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int k = i + 2;
+            for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
+                k = binarySearch(nums, k, nums.length - 1, nums[i] + nums[j]);
+                count += k - j - 1;
+            }
+        }
+        return count;
+    }
+}
