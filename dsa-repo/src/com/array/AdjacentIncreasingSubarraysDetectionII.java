ef2e00d5ced06dff30639214fa757d77421326f2
Ayshani Malik <ayshanim@gmail.com>
2025-10-16 00:49:33 +0530

    initial commit

---
+++ dsa-repo/src/com/array/AdjacentIncreasingSubarraysDetectionII.java
@@ -0,0 +1,60 @@
+package com.array;
+
+import java.util.List;
+
+/*
+3350. Adjacent Increasing Subarrays Detection II
+
+Given an array nums of n integers, your task is to find the maximum value of k for which there exist two adjacent subarrays of length k each, such that both subarrays are strictly increasing. Specifically, check if there are two subarrays of length k starting at indices a and b (a < b), where:
+
+Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
+The subarrays must be adjacent, meaning b = a + k.
+Return the maximum possible value of k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+
+Example 1:
+
+Input: nums = [2,5,7,8,9,2,3,4,3,1]
+
+Output: 3
+
+Explanation:
+
+The subarray starting at index 2 is [7, 8, 9], which is strictly increasing.
+The subarray starting at index 5 is [2, 3, 4], which is also strictly increasing.
+These two subarrays are adjacent, and 3 is the maximum possible value of k for which two such adjacent strictly increasing subarrays exist.
+
+
+TC : o(n)
+SC : o(1)
+ */
+public class AdjacentIncreasingSubarraysDetectionII {
+
+    public static void main(String[] args) {
+        System.out.println(new AdjacentIncreasingSubarraysDetectionII().maxIncreasingSubarrays(
+                List.of(2,5,7,8,9,2,3,4,3,1)
+        ));
+    }
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n = nums.size();
+        int cnt = 1;
+        int precnt = 0;
+        int ans = 0;
+
+        for (int i = 1; i < n; ++i) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                ++cnt;
+            } else {
+                precnt = cnt;
+                cnt = 1;
+            }
+            ans = Math.max(ans, Math.min(precnt, cnt));
+            ans = Math.max(ans, cnt / 2);
+        }
+
+        return ans;
+    }
+}
