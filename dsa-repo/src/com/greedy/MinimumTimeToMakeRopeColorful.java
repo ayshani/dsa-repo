8621b877d802d50de6e820242933e40aee5c3a5e
Ayshani Malik <ayshanim@gmail.com>
2025-11-03 08:18:27 +0530

    initial commit

--- dsa-repo/src/com/greedy/MinimumTimeToMakeRopeColorful.java
+++ dsa-repo/src/com/greedy/MinimumTimeToMakeRopeColorful.java
@@ -30,6 +30,7 @@ public class MinimumTimeToMakeRopeColorful {
     }
     public int minCost(String colors, int[] neededTime) {
         int res =0, sumCost=0,maxCost=0;
+
         for(int i=0;i<colors.length();i++){
             if(i>0 && colors.charAt(i)!=colors.charAt(i-1)){
                 res+=sumCost-maxCost;
@@ -40,6 +41,7 @@ public class MinimumTimeToMakeRopeColorful {
             maxCost= Math.max(maxCost,neededTime[i]);
             System.out.println(i +" "+ sumCost +" "+ maxCost +" "+res);
         }
+
         res+=sumCost-maxCost;
         return res;
     }
