d2fee426665b00f390552bc24af4d4d1f14588a9
Ayshani Malik <ayshanim@gmail.com>
2025-11-14 07:50:58 +0530

    initial commit

---
+++ dsa-repo/src/com/matrix/IncrementSubmatricesByOne.java
@@ -0,0 +1,59 @@
+package com.matrix;
+
+import java.util.Arrays;
+
+/*
+2536. Increment Submatrices by One
+
+You are given a positive integer n, indicating that we initially have an n x n 0-indexed integer matrix mat filled with zeroes.
+
+You are also given a 2D integer array query. For each query[i] = [row1i, col1i, row2i, col2i], you should do the following operation:
+
+Add 1 to every element in the submatrix with the top left corner (row1i, col1i) and the bottom right corner (row2i, col2i). That is, add 1 to mat[x][y] for all row1i <= x <= row2i and col1i <= y <= col2i.
+Return the matrix mat after performing every query.
+
+Example 1:
+Input: n = 3, queries = [[1,1,2,2],[0,0,1,1]]
+Output: [[1,1,0],[1,2,1],[0,1,1]]
+Explanation: The diagram above shows the initial matrix, the matrix after the first query, and the matrix after the second query.
+- In the first query, we add 1 to every element in the submatrix with the top left corner (1, 1) and bottom right corner (2, 2).
+- In the second query, we add 1 to every element in the submatrix with the top left corner (0, 0) and bottom right corner (1, 1).
+
+TC : o(n^2)
+SC: o(n^2)
+ */
+public class IncrementSubmatricesByOne {
+
+    public static void main(String[] args) {
+        int[][] res = new IncrementSubmatricesByOne().rangeAddQueries(
+                3, new int[][]{{1,1,2,2},{0,0,1,1}}
+        );
+
+        Arrays.stream(res).forEach(e -> System.out.println(Arrays.toString(e)));
+    }
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int[][] diff = new int[n + 1][n + 1];
+
+        for (int[] q : queries) {
+            int row1 = q[0];
+            int col1 = q[1];
+            int row2 = q[2];
+            int col2 = q[3];
+            diff[row1][col1] += 1;
+            diff[row2 + 1][col1] -= 1;
+            diff[row1][col2 + 1] -= 1;
+            diff[row2 + 1][col2 + 1] += 1;
+        }
+
+        int[][] mat = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int x1 = (i == 0) ? 0 : mat[i - 1][j];
+                int x2 = (j == 0) ? 0 : mat[i][j - 1];
+                int x3 = (i == 0 || j == 0) ? 0 : mat[i - 1][j - 1];
+                mat[i][j] = diff[i][j] + x1 + x2 - x3;
+            }
+        }
+        return mat;
+    }
+}
