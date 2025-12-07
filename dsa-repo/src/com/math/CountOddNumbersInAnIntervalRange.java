cca883a20e1427e605386d17490b8ae095dc8c44
Ayshani Malik <ayshanim@gmail.com>
2025-12-07 09:01:05 +0530

    initial commit

--- dsa-repo/src/com/math/CountOddNumbersInAnIntervalRange.java
+++ dsa-repo/src/com/math/CountOddNumbersInAnIntervalRange.java
@@ -23,6 +23,7 @@ public class CountOddNumbersInAnIntervalRange {
     public int countOdds(int low, int high) {
         if(low%2==0)
             low++;
+
         if(high%2==0)
             high--;
         return (high-low)/2+1;
