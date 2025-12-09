dfb9aeb826152150a621e5ab0ccc1d3eb5bcdfa1
Ayshani Malik <ayshanim@gmail.com>
2025-12-09 08:25:08 +0530

    initial commit

---
+++ dsa-repo/src/com/array/CountSpecialTriplets.java
@@ -0,0 +1,66 @@
+package com.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+3583. Count Special Triplets
+
+You are given an integer array nums.
+
+A special triplet is defined as a triplet of indices (i, j, k) such that:
+
+0 <= i < j < k < n, where n = nums.length
+nums[i] == nums[j] * 2
+nums[k] == nums[j] * 2
+Return the total number of special triplets in the array.
+
+Since the answer may be large, return it modulo 109 + 7.
+
+
+
+Example 1:
+
+Input: nums = [6,3,6]
+
+Output: 1
+
+Explanation:
+
+The only special triplet is (i, j, k) = (0, 1, 2), where:
+
+nums[0] = 6, nums[1] = 3, nums[2] = 6
+nums[0] = nums[1] * 2 = 3 * 2 = 6
+nums[2] = nums[1] * 2 = 3 * 2 = 6
+
+TC : o(n)
+SC: o(n)
+ */
+public class CountSpecialTriplets {
+
+    public static void main(String[] args) {
+        System.out.println(new CountSpecialTriplets().specialTriplets(new int[]{6,3,6}));
+    }
+    public int specialTriplets(int[] nums) {
+        final int MOD = 1000000007;
+        Map<Integer, Integer> numCnt = new HashMap<>();
+        Map<Integer, Integer> numPartialCnt = new HashMap<>();
+
+        for (int v : nums) {
+            numCnt.put(v, numCnt.getOrDefault(v, 0) + 1);
+        }
+
+        long ans = 0;
+        for (int v : nums) {
+            int target = v * 2;
+            int lCnt = numPartialCnt.getOrDefault(target, 0);
+            numPartialCnt.put(v, numPartialCnt.getOrDefault(v, 0) + 1);
+            int rCnt =
+                    numCnt.getOrDefault(target, 0) -
+                            numPartialCnt.getOrDefault(target, 0);
+            ans = (ans + (long) lCnt * rCnt) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
