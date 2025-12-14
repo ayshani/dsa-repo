7befb2706c4d5e624b2a6abb0c35276b33578749
Ayshani Malik <ayshanim@gmail.com>
2025-12-14 08:37:05 +0530

    initial commit

--- dsa-repo/src/com/array/NumberOfWaysToDivideALongCorridor.java
+++ dsa-repo/src/com/array/NumberOfWaysToDivideALongCorridor.java
@@ -42,6 +42,7 @@ public class NumberOfWaysToDivideALongCorridor {
     }
     public int numberOfWays(String s) {
         long res = 1, j = 0, k = 0, mod = (long)1e9 + 7;
+
         for (int i = 0; i < s.length(); ++i) {
             if (s.charAt(i) == 'S') {
                 if (++k > 2 && k % 2 == 1)
@@ -49,6 +50,7 @@ public class NumberOfWaysToDivideALongCorridor {
                 j = i;
             }
         }
+
         return k % 2 == 0 && k > 0 ? (int)res : 0;
     }
 }
