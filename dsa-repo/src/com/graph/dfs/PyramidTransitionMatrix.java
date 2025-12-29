fc6517af26361a9364a824047bebcaaded0d735b
Ayshani Malik <ayshanim@gmail.com>
2025-12-29 22:46:05 +0530

    initial commit

---
+++ dsa-repo/src/com/graph/dfs/PyramidTransitionMatrix.java
@@ -0,0 +1,95 @@
+package com.graph.dfs;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+756. Pyramid Transition Matrix
+
+You are stacking blocks to form a pyramid. Each block has a color, which is represented by a single letter. Each row of blocks contains one less block than the row beneath it and is centered on top.
+
+To make the pyramid aesthetically pleasing, there are only specific triangular patterns that are allowed. A triangular pattern consists of a single block stacked on top of two blocks. The patterns are given as a list of three-letter strings allowed, where the first two characters of a pattern represent the left and right bottom blocks respectively, and the third character is the top block.
+
+For example, "ABC" represents a triangular pattern with a 'C' block stacked on top of an 'A' (left) and 'B' (right) block. Note that this is different from "BAC" where 'B' is on the left bottom and 'A' is on the right bottom.
+You start with a bottom row of blocks bottom, given as a single string, that you must use as the base of the pyramid.
+
+Given bottom and allowed, return true if you can build the pyramid all the way to the top such that every triangular pattern in the pyramid is in allowed, or false otherwise.
+
+
+
+Example 1:
+Input: bottom = "BCD", allowed = ["BCC","CDE","CEA","FFF"]
+Output: true
+Explanation: The allowed triangular patterns are shown on the right.
+Starting from the bottom (level 3), we can build "CE" on level 2 and then build "A" on level 1.
+There are three triangular patterns in the pyramid, which are "BCC", "CDE", and "CEA". All are allowed.
+
+
+TC : o(A^n)
+SC: o(N^2)
+
+Explanation :
+We can work in either strings or integers, but we need to create a transition map T from the list of allowed triples.
+This map T[x][y] = {set of z} will be all possible parent blocks for a left child of x and a right child of y. When we
+ work in strings, we use Set, and when we work in integers, we will use the set bits of the result integer.
+
+Afterwards, to solve a row, we generate every possible combination of the next row and solve them. If any of those
+new rows are solvable, we return True, otherwise False.
+
+We can also cache intermediate results, saving us time. This is illustrated in the comments for Python. For Java,
+all caching is done with lines of code that mention the integer R.
+ */
+public class PyramidTransitionMatrix {
+
+    int[][] T;
+    Set<Long> seen;
+
+    public static void main(String[] args) {
+        System.out.println(new PyramidTransitionMatrix().pyramidTransition("BCD",
+                List.of("BCC","CDE","CEA","FFF")));
+    }
+
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        T = new int[7][7];
+        for (String a: allowed)
+            T[a.charAt(0) - 'A'][a.charAt(1) - 'A'] |= 1 << (a.charAt(2) - 'A');
+
+        seen = new HashSet();
+        int N = bottom.length();
+        int[][] A = new int[N][N];
+        int t = 0;
+        for (char c: bottom.toCharArray())
+            A[N-1][t++] = c - 'A';
+        return solve(A, 0, N-1, 0);
+    }
+
+    //A[i] - the ith row of the pyramid
+    //R - integer representing the current row of the pyramid
+    //N - length of current row we are calculating
+    //i - index of how far in the current row we are calculating
+    //Returns true iff pyramid can be built
+    public boolean solve(int[][] A, long R, int N, int i) {
+        if (N == 1 && i == 1) { // If successfully placed entire pyramid
+            return true;
+        } else if (i == N) {
+            if (seen.contains(R)) return false; // If we've already tried this row, give up
+            seen.add(R); // Add row to cache
+            return solve(A, 0, N-1, 0); // Calculate next row
+        } else {
+            // w's jth bit is true iff block #j could be
+            // a parent of A[N][i] and A[N][i+1]
+            int w = T[A[N][i]][A[N][i+1]];
+            // for each set bit in w...
+            for (int b = 0; b < 7; ++b) if (((w >> b) & 1) != 0) {
+                A[N-1][i] = b; //set parent to be equal to block #b
+                //If rest of pyramid can be built, return true
+                //R represents current row, now with ith bit set to b+1
+                // in base 8.
+                if (solve(A, R * 8 + (b+1), N, i+1)) return true;
+            }
+            return false;
+        }
+    }
+}
