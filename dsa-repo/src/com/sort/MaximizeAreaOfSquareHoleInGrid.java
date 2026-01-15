852b00c63dbdd17dc43497d72ef772457f8dac83
Ayshani Malik <ayshanim@gmail.com>
2026-01-15 09:50:32 +0530

    initial commit

--- dsa-repo/src/com/sort/MaximizeAreaOfSquareHoleInGrid.java
+++ dsa-repo/src/com/sort/MaximizeAreaOfSquareHoleInGrid.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 /*
 2943. Maximize Area of Square Hole in Grid
 
-You are given the two integers, n and m and two integer arrays, hBars and vBars. The grid has n + 2 horizontal and m + 2 vertical bars, creating 1 x 1 unit cells. The bars are indexed starting from 1.
+You are given the two integers, n and m and two integer arrays, hBars and vBars. The grid has n + 2 horizontal and
+m + 2 vertical bars, creating 1 x 1 unit cells. The bars are indexed starting from 1.
 
-You can remove some of the bars in hBars from horizontal bars and some of the bars in vBars from vertical bars. Note that other bars are fixed and cannot be removed.
+You can remove some of the bars in hBars from horizontal bars and some of the bars in vBars from vertical bars.
+Note that other bars are fixed and cannot be removed.
 
-Return an integer denoting the maximum area of a square-shaped hole in the grid, after removing some bars (possibly none).
+Return an integer denoting the maximum area of a square-shaped hole in the grid, after removing some bars
+(possibly none).
 
 
 
@@ -23,7 +26,8 @@ Output: 4
 
 Explanation:
 
-The left image shows the initial grid formed by the bars. The horizontal bars are [1,2,3,4], and the vertical bars are [1,2,3].
+The left image shows the initial grid formed by the bars. The horizontal bars are [1,2,3,4], and the vertical
+bars are [1,2,3].
 
 One way to get the maximum square-shaped hole is by removing horizontal bar 2 and vertical bar 2.
 
