6c353e02847d14814b8a9db72889d984c64b9b30
Ayshani Malik <ayshanim@gmail.com>
2026-01-22 20:56:13 +0530

    initial commit

---
+++ dsa-repo/src/com/array/MinimumPairRemovalToSortArrayI.java
@@ -0,0 +1,71 @@
+package com.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+3507. Minimum Pair Removal to Sort Array I
+
+Given an array nums, you can perform the following operation any number of times:
+
+Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
+Replace the pair with their sum.
+Return the minimum number of operations needed to make the array non-decreasing.
+
+An array is said to be non-decreasing if each element is greater than or equal to its previous element (if it exists).
+
+
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+
+Output: 2
+
+Explanation:
+
+The pair (3,1) has the minimum sum of 4. After replacement, nums = [5,2,4].
+The pair (2,4) has the minimum sum of 6. After replacement, nums = [5,6].
+The array nums became non-decreasing in two operations.
+
+TC : o(n^2)
+SC: o(1)
+ */
+public class MinimumPairRemovalToSortArrayI {
+
+    public static void main(String[] args) {
+        System.out.println(new MinimumPairRemovalToSortArrayI().minimumPairRemoval(new int[]{5,2,3,1}));
+    }
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums){
+            list.add(num);
+        }
+
+        var count =0;
+        while(list.size()>1){
+            var isAscending = true;
+            var minSum = Integer.MAX_VALUE;
+            var targetIndex =-1;
+            for(int i=0;i<list.size()-1;i++){
+                var sum = list.get(i) + list.get(i+1);
+                if(list.get(i)>list.get(i+1)){
+                    isAscending = false;
+                }
+                if(sum < minSum){
+                    minSum = sum;
+                    targetIndex = i;
+                }
+            }
+
+            if(isAscending){
+                break;
+            }
+
+            count++;
+            list.set(targetIndex, minSum);
+            list.remove(targetIndex+1);
+        }
+        return count;
+    }
+}
