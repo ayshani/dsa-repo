48d9a60309fc56d357b909fb57bca448eb678422
Ayshani Malik <ayshanim@gmail.com>
2026-01-30 08:02:24 +0530

    initial commit

--- dsa-repo/src/com/dp/MinimumCostToConvertStringII.java
+++ dsa-repo/src/com/dp/MinimumCostToConvertStringII.java
@@ -1,2 +1,137 @@
-package com.dp;public class MinimumCostToConvertStringII {
+package com.dp;
+
+import java.util.Arrays;
+
+/*
+2977. Minimum Cost to Convert String II
+
+You are given two 0-indexed strings source and target, both of length n and consisting of lowercase English characters. You are also given two 0-indexed string arrays original and changed, and an integer array cost, where cost[i] represents the cost of converting the string original[i] to the string changed[i].
+
+You start with the string source. In one operation, you can pick a substring x from the string, and change it to y at a cost of z if there exists any index j such that cost[j] == z, original[j] == x, and changed[j] == y. You are allowed to do any number of operations, but any pair of operations must satisfy either of these two conditions:
+
+The substrings picked in the operations are source[a..b] and source[c..d] with either b < c or d < a. In other words, the indices picked in both operations are disjoint.
+The substrings picked in the operations are source[a..b] and source[c..d] with a == c and b == d. In other words, the indices picked in both operations are identical.
+Return the minimum cost to convert the string source to the string target using any number of operations. If it is impossible to convert source to target, return -1.
+
+Note that there may exist indices i, j such that original[j] == original[i] and changed[j] == changed[i].
+
+
+
+Example 1:
+
+Input: source = "abcd", target = "acbe", original = ["a","b","c","c","e","d"],
+changed = ["b","c","b","e","b","e"], cost = [2,5,5,1,2,20]
+Output: 28
+Explanation: To convert "abcd" to "acbe", do the following operations:
+- Change substring source[1..1] from "b" to "c" at a cost of 5.
+- Change substring source[2..2] from "c" to "e" at a cost of 1.
+- Change substring source[2..2] from "e" to "b" at a cost of 2.
+- Change substring source[3..3] from "d" to "e" at a cost of 20.
+The total cost incurred is 5 + 1 + 2 + 20 = 28.
+It can be shown that this is the minimum possible cost.
+
+
+TC : o(n^2+m^2+mL)
+SC: o(n+m^2+mL)
+ */
+public class MinimumCostToConvertStringII {
+
+    public static void main(String[] args) {
+        System.out.println(new MinimumCostToConvertStringII().minimumCost(
+              "abcd",  "acbe",  new String[]{"a","b","c","c","e","d"},
+                new String[]{"b","c","b","e","b","e"}, new int[]{2,5,5,1,2,20}
+        ));
+    }
+    private static final int INF = Integer.MAX_VALUE / 2;
+
+    private int add(Trie node, String word, int[] index) {
+        for (char ch : word.toCharArray()) {
+            int i = ch - 'a';
+            if (node.child[i] == null) {
+                node.child[i] = new Trie();
+            }
+            node = node.child[i];
+        }
+        if (node.id == -1) {
+            node.id = ++index[0];
+        }
+        return node.id;
+    }
+
+    private void update(long[] x, long y) {
+        if (x[0] == -1 || y < x[0]) {
+            x[0] = y;
+        }
+    }
+
+    public long minimumCost(
+            String source,
+            String target,
+            String[] original,
+            String[] changed,
+            int[] cost
+    ) {
+        int n = source.length();
+        int m = original.length;
+        Trie root = new Trie();
+
+        int[] p = { -1 };
+        int[][] G = new int[m * 2][m * 2];
+
+        for (int i = 0; i < m * 2; i++) {
+            Arrays.fill(G[i], INF);
+            G[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int x = add(root, original[i], p);
+            int y = add(root, changed[i], p);
+            G[x][y] = Math.min(G[x][y], cost[i]);
+        }
+
+        int size = p[0] + 1;
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    G[i][j] = Math.min(G[i][j], G[i][k] + G[k][j]);
+                }
+            }
+        }
+
+        long[] f = new long[n];
+        Arrays.fill(f, -1);
+        for (int j = 0; j < n; j++) {
+            if (j > 0 && f[j - 1] == -1) {
+                continue;
+            }
+            long base = (j == 0 ? 0 : f[j - 1]);
+            if (source.charAt(j) == target.charAt(j)) {
+                f[j] = f[j] == -1 ? base : Math.min(f[j], base);
+            }
+
+            Trie u = root;
+            Trie v = root;
+            for (int i = j; i < n; i++) {
+                u = u.child[source.charAt(i) - 'a'];
+                v = v.child[target.charAt(i) - 'a'];
+                if (u == null || v == null) {
+                    break;
+                }
+                if (u.id != -1 && v.id != -1 && G[u.id][v.id] != INF) {
+                    long newVal = base + G[u.id][v.id];
+                    if (f[i] == -1 || newVal < f[i]) {
+                        f[i] = newVal;
+                    }
+                }
+            }
+        }
+
+        return f[n - 1];
+    }
+}
+
+class Trie {
+
+    Trie[] child = new Trie[26];
+    int id = -1;
 }
