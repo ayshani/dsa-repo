8e053eec9f3c3abb1946f786eb18005c3d65c66c
Ayshani Malik <ayshanim@gmail.com>
2026-02-17 16:07:07 +0530

    initial commit

---
+++ dsa-repo/src/com/array/BinaryWatch.java
@@ -0,0 +1,45 @@
+package com.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+401. Binary Watch
+
+A binary watch has 4 LEDs on the top to represent the hours (0-11), and 6 LEDs on the bottom to represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
+
+For example, the below binary watch reads "4:51".
+H 8 4 2 1 PM
+M 32 16 8 4 2 1
+Given an integer turnedOn which represents the number of LEDs that are currently on (ignoring the PM), return all possible times the watch could represent. You may return the answer in any order.
+
+The hour must not contain a leading zero.
+
+For example, "01:00" is not valid. It should be "1:00".
+The minute must consist of two digits and may contain a leading zero.
+
+For example, "10:2" is not valid. It should be "10:02".
+
+
+Example 1:
+
+Input: turnedOn = 1
+Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
+ */
+public class BinaryWatch {
+
+    public static void main(String[] args) {
+        System.out.println(new BinaryWatch().readBinaryWatch(5));
+    }
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<String>();
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    ans.add(h + ":" + (m < 10 ? "0" : "") + m);
+                }
+            }
+        }
+        return ans;
+    }
+}
