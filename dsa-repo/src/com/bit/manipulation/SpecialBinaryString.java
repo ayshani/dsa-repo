a04ec0ebb39eedfad8b408e5e9ecefb4af43611a
Ayshani Malik <ayshanim@gmail.com>
2026-02-21 21:51:34 +0530

    initial commit

---
+++ dsa-repo/src/com/bit/manipulation/SpecialBinaryString.java
@@ -0,0 +1,48 @@
+package com.bit.manipulation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*761. Special Binary String
+Special binary strings are binary strings with the following two properties:
+
+The number of 0's is equal to the number of 1's.
+Every prefix of the binary string has at least as many 1's as 0's.
+You are given a special binary string s.
+
+A move consists of choosing two consecutive, non-empty, special substrings of s, and swapping them. Two strings are consecutive if the last character of the first string is exactly one index before the first character of the second string.
+
+Return the lexicographically largest resulting string possible after applying the mentioned operations on the string.
+
+
+
+Example 1:
+
+Input: s = "11011000"
+Output: "11100100"
+Explanation: The strings "10" [occuring at s[1]] and "1100" [at s[3]] are swapped.
+This is the lexicographically largest string possible after some number of swaps.
+
+
+ */
+public class SpecialBinaryString {
+
+    public static void main(String[] args) {
+        System.out.println(new SpecialBinaryString().makeLargestSpecial("11011000"));
+    }
+    public String makeLargestSpecial(String S) {
+        int count = 0, i = 0;
+        List<String> res = new ArrayList<String>();
+        for (int j = 0; j < S.length(); ++j) {
+            if (S.charAt(j) == '1') count++;
+            else count--;
+            if (count == 0) {
+                res.add('1' + makeLargestSpecial(S.substring(i + 1, j)) + '0');
+                i = j + 1;
+            }
+        }
+        Collections.sort(res, Collections.reverseOrder());
+        return String.join("", res);
+    }
+}
