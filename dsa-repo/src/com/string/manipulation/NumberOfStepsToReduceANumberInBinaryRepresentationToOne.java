95537939f74ccf1429db15dd74db5d7270e46fa0
Ayshani Malik <ayshanim@gmail.com>
2026-02-26 20:50:01 +0530

    initial commit

--- dsa-repo/src/com/string/manipulation/NumberOfStepsToReduceANumberInBinaryRepresentationToOne.java
+++ dsa-repo/src/com/string/manipulation/NumberOfStepsToReduceANumberInBinaryRepresentationToOne.java
@@ -36,6 +36,7 @@ public class NumberOfStepsToReduceANumberInBinaryRepresentationToOne {
         ));
     }
     public int numSteps(String s) {
+
         int n = s.length();
         int operations =0, carry =0;
         for(int i= n-1; i> 0; i--){
