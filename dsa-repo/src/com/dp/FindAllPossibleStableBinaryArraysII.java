831ed2dee37c441ef83c28551122b570b3c84316
Ayshani Malik <ayshanim@gmail.com>
2026-03-10 08:02:11 +0530

    initial commit

---
+++ dsa-repo/src/com/dp/FindAllPossibleStableBinaryArraysII.java
@@ -0,0 +1,78 @@
+package com.dp;
+
+import java.util.Arrays;
+
+/*
+3130. Find All Possible Stable Binary Arrays II
+
+You are given 3 positive integers zero, one, and limit.
+
+A binary array arr is called stable if:
+
+The number of occurrences of 0 in arr is exactly zero.
+The number of occurrences of 1 in arr is exactly one.
+Each subarray of arr with a size greater than limit must contain both 0 and 1.
+Return the total number of stable binary arrays.
+
+Since the answer may be very large, return it modulo 109 + 7.
+
+
+
+Example 1:
+
+Input: zero = 1, one = 1, limit = 2
+
+Output: 2
+
+Explanation:
+
+The two possible stable binary arrays are [1,0] and [0,1].
+
+TC : o(zero*one)
+SC : o(zero*one)
+ */
+public class FindAllPossibleStableBinaryArraysII {
+
+    public static void main(String[] args) {
+        System.out.println(new FindAllPossibleStableBinaryArraysII().numberOfStableArrays(1,2,1));
+    }
+    static final int MOD = 1000000007;
+    int[][][] memo;
+    int limit;
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        this.memo = new int[zero + 1][one + 1][2];
+        for (int i = 0; i <= zero; i++) {
+            for (int j = 0; j <= one; j++) {
+                Arrays.fill(memo[i][j], -1);
+            }
+        }
+        this.limit = limit;
+        return (dp(zero, one, 0) + dp(zero, one, 1)) % MOD;
+    }
+
+    public int dp(int zero, int one, int lastBit) {
+        if (zero == 0) {
+            return (lastBit == 0 || one > limit) ? 0 : 1;
+        } else if (one == 0) {
+            return (lastBit == 1 || zero > limit) ? 0 : 1;
+        }
+
+        if (memo[zero][one][lastBit] == -1) {
+            int res = 0;
+            if (lastBit == 0) {
+                res = (dp(zero - 1, one, 0) + dp(zero - 1, one, 1)) % MOD;
+                if (zero > limit) {
+                    res = (res - dp(zero - limit - 1, one, 1) + MOD) % MOD;
+                }
+            } else {
+                res = (dp(zero, one - 1, 0) + dp(zero, one - 1, 1)) % MOD;
+                if (one > limit) {
+                    res = (res - dp(zero, one - limit - 1, 0) + MOD) % MOD;
+                }
+            }
+            memo[zero][one][lastBit] = res % MOD;
+        }
+        return memo[zero][one][lastBit];
+    }
+}
