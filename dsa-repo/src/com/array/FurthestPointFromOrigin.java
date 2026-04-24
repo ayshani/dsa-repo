1f8337b31ba283664883a13cbf810b38ccb65765
Ayshani Malik <ayshanim@gmail.com>
2026-04-24 22:39:00 +0530

    initial commit

--- dsa-repo/src/com/array/FurthestPointFromOrigin.java
+++ dsa-repo/src/com/array/FurthestPointFromOrigin.java
@@ -30,6 +30,7 @@ public class FurthestPointFromOrigin {
     }
     public int furthestDistanceFromOrigin(String moves) {
         int countL =0, countR =0, countSpace =0;
+
         for(int i=0;i<moves.length();i++){
             if(moves.charAt(i)=='L')
                 countL++;
