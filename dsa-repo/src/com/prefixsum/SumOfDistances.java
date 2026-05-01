2075d8948a460da71b20ef894012c4b0e0e4ba2e
Ayshani Malik <ayshanim@gmail.com>
2026-05-01 22:51:01 +0530

    initial commit

---
+++ dsa-repo/src/com/prefixsum/SumOfDistances.java
@@ -0,0 +1,54 @@
+package com.prefixsum;
+
+import java.util.*;
+
+/*
+2615. Sum of Distances
+
+You are given a 0-indexed integer array nums. There exists an array arr of length nums.length, where arr[i] is the sum of |i - j| over all j such that nums[j] == nums[i] and j != i. If there is no such j, set arr[i] to be 0.
+
+Return the array arr.
+
+
+
+Example 1:
+
+Input: nums = [1,3,1,1,2]
+Output: [5,0,3,4,0]
+Explanation:
+When i = 0, nums[0] == nums[2] and nums[0] == nums[3]. Therefore, arr[0] = |0 - 2| + |0 - 3| = 5.
+When i = 1, arr[1] = 0 because there is no other index with value 3.
+When i = 2, nums[2] == nums[0] and nums[2] == nums[3]. Therefore, arr[2] = |2 - 0| + |2 - 3| = 3.
+When i = 3, nums[3] == nums[0] and nums[3] == nums[2]. Therefore, arr[3] = |3 - 0| + |3 - 2| = 4.
+When i = 4, arr[4] = 0 because there is no other index with value 2.
+
+
+ */
+public class SumOfDistances {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new SumOfDistances().distance(new int[]{1,3,1,1,2})));
+    }
+    public long[] distance(int[] nums) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            groups.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        long[] res = new long[n];
+        for (List<Integer> group : groups.values()) {
+            long total = 0;
+            for (int idx : group) {
+                total += idx;
+            }
+            long prefixTotal = 0;
+            int sz = group.size();
+            for (int i = 0; i < sz; i++) {
+                int idx = group.get(i);
+                res[idx] = total - prefixTotal * 2 + (long) idx * (2 * i - sz);
+                prefixTotal += idx;
+            }
+        }
+        return res;
+    }
+}
