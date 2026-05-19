fa557612ac8aa1434af66222deeb10c0ee592f19
Ayshani Malik <ayshanim@gmail.com>
2026-05-19 22:36:35 +0530

    initial commit

--- dsa-repo/src/com/array/MinimumCommonValue.java
+++ dsa-repo/src/com/array/MinimumCommonValue.java
@@ -27,6 +27,7 @@ public class MinimumCommonValue {
     public int getCommon(int[] nums1, int[] nums2) {
         int i=0, j =0;
         int m = nums1.length, n = nums2.length;
+
         while(i<m && j<n){
             if(nums1[i]==nums2[j]){
                 return nums1[i];
@@ -37,6 +38,5 @@ public class MinimumCommonValue {
             }
         }
         return -1;
-
     }
 }
