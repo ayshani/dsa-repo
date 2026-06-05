0dc0959eebecf81d5868751fd5549d32521eaa9e
Ayshani Malik <ayshanim@gmail.com>
2026-06-05 18:06:09 +0530

    initial commit

---
+++ dsa-repo/src/com/dp/TotalWavinessOfNumbersInRangeII.java
@@ -0,0 +1,140 @@
+package com.dp;
+
+import java.util.Arrays;
+
+/*
+3753. Total Waviness of Numbers in Range II
+
+You are given two integers num1 and num2 representing an inclusive range [num1, num2].
+
+The waviness of a number is defined as the total count of its peaks and valleys:
+
+A digit is a peak if it is strictly greater than both of its immediate neighbors.
+A digit is a valley if it is strictly less than both of its immediate neighbors.
+The first and last digits of a number cannot be peaks or valleys.
+Any number with fewer than 3 digits has a waviness of 0.
+Return the total sum of waviness for all numbers in the range [num1, num2].
+
+
+Example 1:
+
+Input: num1 = 120, num2 = 130
+
+Output: 3
+
+Explanation:
+
+In the range [120, 130]:
+
+120: middle digit 2 is a peak, waviness = 1.
+121: middle digit 2 is a peak, waviness = 1.
+130: middle digit 3 is a peak, waviness = 1.
+All other numbers in the range have a waviness of 0.
+Thus, total waviness is 1 + 1 + 1 = 3.
+
+TC : o(D^3lognum2)
+SC : o(D^2lognum2)
+ */
+public class TotalWavinessOfNumbersInRangeII {
+
+    public static void main(String[] args) {
+        System.out.println(new TotalWavinessOfNumbersInRangeII().totalWaviness(120,130));
+    }
+    private String s;
+    private int n;
+    private long[][][] memo_cnt;
+    private long[][][] memo_sum;
+
+    public long totalWaviness(long num1, long num2) {
+        return solve(num2) - solve(num1 - 1);
+    }
+
+    // calculate the sum of fluctuation values of all numbers in the range [0, num]
+    private long solve(long num) {
+        // if the fluctuation value of numbers less than 3 is 0
+        if (num < 100) {
+            return 0L;
+        }
+        s = Long.toString(num);
+        n = s.length();
+
+        // memoized search uses two independent arrays
+        // memo_cnt[pos][x][y]: the number of valid filling schemes where the current digit is at position pos, and the previous two digits are x and y
+        memo_cnt = new long[16][10][10];
+        // memo_sum[pos][x][y]: the fluctuation value when the current position is pos, and the two left digits are x and y
+        memo_sum = new long[16][10][10];
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 10; j++) {
+                Arrays.fill(memo_cnt[i][j], -1);
+                Arrays.fill(memo_sum[i][j], -1);
+            }
+        }
+
+        long[] result = dfs(0, -1, -1, true, true);
+        return result[1];
+    }
+
+    private long[] dfs(
+            int pos,
+            int prev,
+            int curr,
+            boolean isLimit,
+            boolean isLeading
+    ) {
+        // end position
+        if (pos == n) {
+            return new long[] { 1L, 0L };
+        }
+        // use memoization only when not bounded by an upper limit and without leading zeros
+        if (!isLimit && !isLeading && prev >= 0 && curr >= 0) {
+            if (memo_cnt[pos][prev][curr] != -1) {
+                return new long[] {
+                        memo_cnt[pos][prev][curr],
+                        memo_sum[pos][prev][curr],
+                };
+            }
+        }
+
+        // calculate the number of schemes and fluctuation value under the current conditions
+        long cnt = 0;
+        long sum = 0;
+        int up = isLimit ? (s.charAt(pos) - '0') : 9;
+        for (int digit = 0; digit <= up; ++digit) {
+            boolean newLeading = isLeading && (digit == 0);
+            // the previous number is updated to curr
+            int newPrev = curr;
+            // the current number is updated to digit
+            int newCurr = newLeading ? -1 : digit;
+            long[] sub = dfs(
+                    pos + 1,
+                    newPrev,
+                    newCurr,
+                    isLimit && (digit == up),
+                    newLeading
+            );
+            long subCnt = sub[0];
+            long subSum = sub[1];
+            // only calculate the fluctuation value when there are no leading zeros
+            if (!newLeading && prev >= 0 && curr >= 0) {
+                // when the digit is a peak or a valley, update the current fluctuation value
+                if (
+                        (prev < curr && curr > digit) ||
+                                (prev > curr && curr < digit)
+                ) {
+                    sum += subCnt;
+                }
+            }
+
+            cnt += subCnt;
+            sum += subSum;
+        }
+
+        if (!isLimit && !isLeading && prev >= 0 && curr >= 0) {
+            // update the memoization array
+            memo_cnt[pos][prev][curr] = cnt;
+            memo_sum[pos][prev][curr] = sum;
+        }
+
+        return new long[] { cnt, sum };
+    }
+}
