110ed88ad23424dd58dc20ef8133a8c7458df5d7
Ayshani Malik <ayshanim@gmail.com>
2026-06-21 09:25:45 +0530

    initial commit

--- dsa-repo/src/com/greedy/MaximumIceCreamBars.java
+++ dsa-repo/src/com/greedy/MaximumIceCreamBars.java
@@ -35,6 +35,7 @@ public class MaximumIceCreamBars {
     public int maxIceCream(int[] costs, int coins) {
         Arrays.sort(costs);
         int sum =0, count=0;
+
         for(int i=0;i<costs.length;i++){
             if(sum+costs[i]<=coins){
                 sum+= costs[i];
