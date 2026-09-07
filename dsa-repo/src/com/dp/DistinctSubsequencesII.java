fd9f09a3938289ac92c424249d0adf716bd41df6
Ayshani Malik <ayshanim@gmail.com>
2026-09-07 23:47:28 +0530

    initial commit

---
+++ dsa-repo/src/com/dp/DistinctSubsequencesII.java
@@ -0,0 +1,54 @@
+package com.dp;
+
+import java.util.Arrays;
+
+/*
+940. Distinct Subsequences II
+
+Given a string s, return the number of distinct non-empty subsequences of s. Since the answer may be very large, return it modulo 109 + 7.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not.
+
+
+Example 1:
+
+Input: s = "abc"
+Output: 7
+Explanation: The 7 distinct subsequences are "a", "b", "c", "ab", "ac", "bc", and "abc".
+Example 2:
+
+Input: s = "aba"
+Output: 6
+Explanation: The 6 distinct subsequences are "a", "b", "ab", "aa", "ba", and "aba".
+
+TC : o(n)
+SC: o(n)
+ */
+public class DistinctSubsequencesII {
+
+    public static void main(String[] args) {
+        System.out.println(new DistinctSubsequencesII().distinctSubseqII("abc"));
+    }
+    public int distinctSubseqII(String S) {
+        int MOD = 1_000_000_007;
+        int N = S.length();
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+
+        int[] last = new int[26];
+        Arrays.fill(last, -1);
+
+        for (int i = 0; i < N; ++i) {
+            int x = S.charAt(i) - 'a';
+            dp[i+1] = dp[i] * 2 % MOD;
+            if (last[x] >= 0)
+                dp[i+1] -= dp[last[x]];
+            dp[i+1] %= MOD;
+            last[x] = i;
+        }
+
+        dp[N]--;
+        if (dp[N] < 0) dp[N] += MOD;
+        return dp[N];
+    }
+}
