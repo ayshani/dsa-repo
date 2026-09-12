3bd8bbde809294d2f5ddc8f742c1857660a36ce6
Ayshani Malik <ayshanim@gmail.com>
2026-09-12 22:15:07 +0530

    initial commit

--- dsa-repo/src/com/dp/MaximumScoreOfNonOverlappingIntervals.java
+++ dsa-repo/src/com/dp/MaximumScoreOfNonOverlappingIntervals.java
@@ -8,11 +8,15 @@ import java.util.List;
 /*
 3414. Maximum Score of Non-overlapping Intervals
 
-You are given a 2D integer array intervals, where intervals[i] = [li, ri, weighti]. Interval i starts at position li and ends at ri, and has a weight of weighti. You can choose up to 4 non-overlapping intervals. The score of the chosen intervals is defined as the total sum of their weights.
+You are given a 2D integer array intervals, where intervals[i] = [li, ri, weighti]. Interval i starts at position
+li and ends at ri, and has a weight of weighti. You can choose up to 4 non-overlapping intervals. The score of the
+chosen intervals is defined as the total sum of their weights.
 
-Return the lexicographically smallest array of at most 4 indices from intervals with maximum score, representing your choice of non-overlapping intervals.
+Return the lexicographically smallest array of at most 4 indices from intervals with maximum score, representing your
+choice of non-overlapping intervals.
 
-Two intervals are said to be non-overlapping if they do not share any points. In particular, intervals sharing a left or right boundary are considered overlapping.
+Two intervals are said to be non-overlapping if they do not share any points. In particular, intervals sharing a left
+or right boundary are considered overlapping.
 
 
 
